0ba5774e9e4c59d4ce2c2d7d527c740f9947d25c
dodd <358683537@qq.com>
2021-09-08 17:13:57 +0800

    feat: add MASA.Contrib.Service.MinimalAPIs

---
+++ src/Service/MASA.Contrib.Service.MinimalAPIs/ServiceBase.cs
@@ -0,0 +1,16 @@
+﻿using MASA.BuildingBlocks.Service.MinimalAPIs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MASA.Contrib.Service.MinimalAPIs;
+public class ServiceBase : IService
+{
+    public IServiceCollection Services { get; protected set}
+
+    public ServiceBase(IServiceCollection services) => Services = services;
+
+    public TService? GetService<TService>() => Services.BuildServiceProvider().GetService<TService>();
+
+    public TService GetRequiredService<TService>()
+        where TService : notnull
+        => Services.BuildServiceProvider().GetRequiredService<TService>();
+}
