f5a1ddb3f51970bba6f38d69aee5dfdef492720e
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-18 12:12:43 +0800

    test(EventBus): Added unit test for EventBus Cannel relationship error

---
+++ test/Masa.Contrib.Dispatcher.Events.HandlerOrder.Tests/Events/CalculateEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Masa.Contrib.Dispatcher.Events.HandlerOrder.Tests.Events;
+
+public record CalculateEvent : Event
+{
+    public int ParameterA { get; set; }
+
+    public int ParameterB { get; set; }
+
+    public int Result { get; set; }
+}
---
+++ test/Masa.Contrib.Dispatcher.Events.HandlerOrder.Tests/_Imports.cs
@@ -0,0 +1,1 @@
+global using Masa.Contrib.Dispatcher.Events.HandlerOrder.Tests.Events;
