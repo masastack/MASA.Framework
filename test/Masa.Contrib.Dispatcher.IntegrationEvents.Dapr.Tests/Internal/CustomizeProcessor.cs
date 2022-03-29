c45bc0dd3128b6d63b86563b896f18f60f286101
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-29 20:00:40 +0800

    test: Adapt unit tests to new background tasks

--- test/Masa.Contrib.Dispatcher.IntegrationEvents.Dapr.Tests/Internal/CustomizeProcessor.cs
+++ test/Masa.Contrib.Dispatcher.IntegrationEvents.Dapr.Tests/Internal/CustomizeProcessor.cs
@@ -3,12 +3,16 @@
 public class CustomizeProcessor : ProcessorBase
 {
     internal static int Times = 0;
-    
+
+    public override int Delay => 2;
+
+    public CustomizeProcessor(IServiceProvider? serviceProvider) : base(serviceProvider)
+    {
+    }
+
     public override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         Times++;
         return Task.CompletedTask;
     }
-
-    public override int Delay => 2;
 }
