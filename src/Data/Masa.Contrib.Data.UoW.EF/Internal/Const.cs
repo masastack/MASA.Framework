c0249046b2b82bcd6a7bb8580ca6ac1c66ca94f8
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-13 17:27:52 +0800

    feat(UoW): support using MasaConfiguration

---
+++ src/Data/Masa.Contrib.Data.UoW.EF/Internal/Const.cs
@@ -0,0 +1,6 @@
+﻿namespace Masa.Contrib.Data.UoW.EF.Internal;
+
+internal class Const
+{
+    public const string DEFAULT_SECTION = "ConnectionStrings";
+}
--- src/Data/Masa.Contrib.Data.UoW.EF/_Imports.cs
+++ src/Data/Masa.Contrib.Data.UoW.EF/_Imports.cs
@@ -1,9 +1,12 @@
+global using Masa.BuildingBlocks.Configuration;
 global using Masa.BuildingBlocks.Data.UoW;
 global using Masa.BuildingBlocks.Data.UoW.Options;
 global using Masa.BuildingBlocks.Dispatcher.Events;
+global using Masa.Contrib.Data.UoW.EF.Internal;
 global using Masa.Utils.Data.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Logging;
@@ -12,3 +15,4 @@ global using System.Data.Common;
 global using System.Text.Json.Serialization;
 global using EntityState = Masa.BuildingBlocks.Data.UoW.EntityState;
 global using DbContextOptions = Masa.BuildingBlocks.Data.UoW.Options.MasaDbContextConfigurationOptions;
+
