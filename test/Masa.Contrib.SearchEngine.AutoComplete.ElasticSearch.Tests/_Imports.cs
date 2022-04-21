2b408d9cf4c65ada40c73c7dd7524f2a909decf2
听雨声 <wangzhenlei520@gmail.com>
2022-04-21 11:09:06 +0800

    chore: Masa.Contrib.SearchEngine.AutoComplete.Tests rename to Masa.Contrib.SearchEngine.AutoComplete.ElasticSearch.Tests (#45)

--- test/Masa.Contrib.SearchEngine.AutoComplete.ElasticSearch.Tests/_Imports.cs
+++ test/Masa.Contrib.SearchEngine.AutoComplete.ElasticSearch.Tests/_Imports.cs
@@ -1,6 +1,6 @@
 global using Masa.BuildingBlocks.SearchEngine.AutoComplete;
 global using Masa.BuildingBlocks.SearchEngine.AutoComplete.Options;
-global using Masa.Contrib.SearchEngine.AutoComplete.Tests.Model;
+global using Masa.Contrib.SearchEngine.AutoComplete.ElasticSearch.Tests.Model;
 global using Masa.Utils.Data.Elasticsearch;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
