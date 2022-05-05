9eb0e4bf7446f3fdb66d8febd1ca7781565af6f9
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-05 10:56:32 +0800

    chore: Modify global using sorting

--- src/Data/Mapping/Masa.Contrib.Data.Mapping.Mapster/_Imports.cs
+++ src/Data/Mapping/Masa.Contrib.Data.Mapping.Mapster/_Imports.cs
@@ -1,12 +1,15 @@
-﻿global using System.Reflection;
-global using System.Linq.Expressions;
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
 global using Mapster;
+global using Mapster.Models;
 global using Masa.BuildingBlocks.Data.Mapping;
 global using Masa.BuildingBlocks.Data.Mapping.Options;
-global using System.Collections.Concurrent;
-global using Mapster.Models;
 global using Masa.BuildingBlocks.Data.Mapping.Options.Enum;
 global using Masa.Contrib.Data.Mapping.Mapster.Internal;
 global using Masa.Contrib.Data.Mapping.Mapster.Internal.Options;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using System.Collections.Concurrent;
+global using System.Linq.Expressions;
+global using System.Reflection;
--- test/Masa.Contrib.Data.Mapping.Mapster.Tests/_Imports.cs
+++ test/Masa.Contrib.Data.Mapping.Mapster.Tests/_Imports.cs
@@ -2,15 +2,15 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Mapster;
+global using Masa.BuildingBlocks.Data.Mapping;
+global using Masa.Contrib.Data.Mapping.Mapster.Tests.Domain.Aggregates.Orders;
+global using Masa.Contrib.Data.Mapping.Mapster.Tests.Domain.Aggregates.Users;
+global using Masa.Contrib.Data.Mapping.Mapster.Tests.Domain.ValueObjects;
+global using Masa.Contrib.Data.Mapping.Mapster.Tests.Requests;
+global using Masa.Contrib.Data.Mapping.Mapster.Tests.Requests.Orders;
+global using Masa.Contrib.Data.Mapping.Mapster.Tests.Requests.Users;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using Masa.Contrib.Data.Mapping.Mapster.Tests.Domain.Aggregates.Orders;
-global using Masa.Contrib.Data.Mapping.Mapster.Tests.Domain.Aggregates.Users;
-global using Masa.Contrib.Data.Mapping.Mapster.Tests.Requests;
-global using Masa.Contrib.Data.Mapping.Mapster.Tests.Requests.Orders;
-global using Masa.Contrib.Data.Mapping.Mapster.Tests.Requests.Users;
-global using Masa.Contrib.Data.Mapping.Mapster.Tests.Domain.ValueObjects;
-global using Masa.BuildingBlocks.Data.Mapping;
