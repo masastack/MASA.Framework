36a2e37e604fc206ecd88f3dc430382170dbedb8
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-25 18:39:27 +0800

    chore: Adjust namespace order

--- src/Data/IdGenerator/Masa.Contrib.Data.IdGenerator.Snowflake.Distributed.Redis/_Imports.cs
+++ src/Data/IdGenerator/Masa.Contrib.Data.IdGenerator.Snowflake.Distributed.Redis/_Imports.cs
@@ -1,14 +1,14 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
+// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+global using Masa.BuildingBlocks.Data;
+global using Masa.Contrib.Data.IdGenerator.Snowflake.Distributed.Redis.Internal;
+global using Masa.Utils.Caching.Core.Interfaces;
+global using Masa.Utils.Caching.Redis;
+global using Masa.Utils.Caching.Redis.Models;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
-global using Masa.Utils.Caching.Redis.Models;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using StackExchange.Redis;
-global using Masa.BuildingBlocks.Data;
-global using Masa.Utils.Caching.Core.Interfaces;
-global using Masa.Utils.Caching.Redis;
 global using System.Globalization;
-global using Masa.Contrib.Data.IdGenerator.Snowflake.Distributed.Redis.Internal;
