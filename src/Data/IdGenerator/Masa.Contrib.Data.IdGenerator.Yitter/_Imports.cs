e35079fc897060a0ac50a9cf1cad7069ce4946fd
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-25 09:44:52 +0800

    chore: Delete the IdGenerator.Yitter class library

--- src/Data/IdGenerator/Masa.Contrib.Data.IdGenerator.Yitter/_Imports.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.DependencyInjection.Extensions;
-global using Yitter.IdGenerator;
-global using IIdGenerator = Masa.BuildingBlocks.Data.IIdGenerator;
-global using YitterIIdGenerator = Yitter.IdGenerator.IIdGenerator;
