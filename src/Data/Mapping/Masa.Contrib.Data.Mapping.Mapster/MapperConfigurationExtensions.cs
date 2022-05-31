a49a642e27280aa36a4a300c6df57a8240c7d5fb
zhenlei520 <wangzhenlei520@gmail.com>
2022-05-31 11:37:15 +0800

    feat(Mapping.Mapster): Extend object to support mapping

---
+++ src/Data/Mapping/Masa.Contrib.Data.Mapping.Mapster/MapperConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Data.Mapping.Mapster;
+
+public static class MapperConfigurationExtensions
+{
+    public static MapperConfiguration AddMapping(this MapperConfiguration mapperConfiguration)
+        => mapperConfiguration.AddMapping(MapMode.Shared);
+
+    public static MapperConfiguration AddMapping(this MapperConfiguration mapperConfiguration, MapMode mode)
+        => mapperConfiguration.AddMapping(new MapOptions()
+        {
+            Mode = mode
+        });
+
+    public static MapperConfiguration AddMapping(this MapperConfiguration mapperConfiguration, MapOptions mapOptions)
+    {
+        mapperConfiguration.ConfigurationMapperOptions(new MapsterOptionsExtension(mapOptions));
+        return mapperConfiguration;
+    }
+}
