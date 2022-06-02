1c4326d997d9a1107a0e8f6fd338935e2bfeb311
zhenlei520 <wangzhenlei520@gmail.com>
2022-06-02 16:42:59 +0800

    feat(IdGenerator): Add SimpleGuidGenerator and SequentialGuidGenerator

---
+++ src/Data/IdGenerator/Masa.Contrib.Data.IdGenerator.SequentialGuids/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Data.IdGenerator.SequentialGuids;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSequentialGuidGenerator(this IServiceCollection services)
+        => services.AddSequentialGuidGenerator(SequentialGuidType.SequentialAtEnd);
+
+    public static IServiceCollection AddSequentialGuidGenerator(this IServiceCollection services, SequentialGuidType guidType)
+    {
+        services.TryAddSingleton<ISequentialGuidGenerator>(_ => new SequentialGuidGenerator(guidType));
+        services.TryAddSingleton<IIdGenerator<SequentialGuid, Guid>>(serviceProvider
+            => serviceProvider.GetRequiredService<ISequentialGuidGenerator>());
+        IdGeneratorFactory.SetSequentialGuidGenerator(services.BuildServiceProvider().GetRequiredService<ISequentialGuidGenerator>());
+        return services;
+    }
+}
---
+++ src/Data/IdGenerator/Masa.Contrib.Data.IdGenerator.SimpleGuid/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Data.IdGenerator.SimpleGuid;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSimpleGuidGenerator(this IServiceCollection services)
+    {
+        services.TryAddSingleton<IGuidGenerator, SimpleGuidGenerator>();
+        services.TryAddSingleton<IIdGenerator<Guid, Guid>>(serviceProvider => serviceProvider.GetRequiredService<IGuidGenerator>());
+        IdGeneratorFactory.SetGuidGenerator(services.BuildServiceProvider().GetRequiredService<IGuidGenerator>());
+        return services;
+    }
+}
