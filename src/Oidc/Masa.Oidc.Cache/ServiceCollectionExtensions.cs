e369dfff2e9336c22100dc6294824b2e70e17031
Mayue <mayue@lonsid.cn>
2022-06-09 17:24:06 +0800

    feat:add redis cache options

--- src/Oidc/Masa.Oidc.Cache/ServiceCollectionExtensions.cs
+++ src/Oidc/Masa.Oidc.Cache/ServiceCollectionExtensions.cs
@@ -1,12 +1,15 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+using Masa.Utils.Caching.Redis.Models;
+
 namespace Masa.Oidc.Cache;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddOidcCache(this IServiceCollection services)
+    public static IServiceCollection AddOidcCache(this IServiceCollection services, RedisConfigurationOptions options)
     {
+        services.AddMasaRedisCache(options).AddMasaMemoryCache();
         services.AddSingleton<IClientCache, ClientCache>();
         services.AddSingleton<IApiScopeCache, ApiScopeCache>();
         services.AddSingleton<IApiResourceCache, ApiResourceCache>();
--- src/Oidc/Masa.Oidc.Cache/_import.cs
+++ src/Oidc/Masa.Oidc.Cache/_import.cs
@@ -1,9 +1,12 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-global using Masa.Utils.Caching.DistributedMemory.Interfaces;
-global using Masa.Oidc.Cache.Models;
-global using Masa.BuildingBlocks.Oidc.Domain.Entities;
 global using Masa.BuildingBlocks.Oidc.Cache.Caches;
-global using Microsoft.Extensions.DependencyInjection;
+global using Masa.BuildingBlocks.Oidc.Domain.Entities;
 global using Masa.Oidc.Cache.Caches;
+global using Masa.Oidc.Cache.Models;
+global using Masa.Utils.Caching.DistributedMemory.DependencyInjection;
+global using Masa.Utils.Caching.DistributedMemory.Interfaces;
+global using Masa.Utils.Caching.Redis.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
