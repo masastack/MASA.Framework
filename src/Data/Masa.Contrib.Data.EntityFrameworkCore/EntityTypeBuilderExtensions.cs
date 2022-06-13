d18c83bfabc4789e56be9770529851ff2efb061c
zhenlei520 <wangzhenlei520@gmail.com>
2022-06-13 11:29:02 +0800

    chore: Add the ConcurrencyStamp extension to make it easier to set up mappings

---
+++ src/Data/Masa.Contrib.Data.EntityFrameworkCore/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Data.EntityFrameworkCore;
+
+public static class EntityTypeBuilderExtensions
+{
+    private const int _maxLength = 36;
+
+    public static void TryConfigureConcurrencyStamp(
+        this EntityTypeBuilder entityTypeBuilder,
+        string? propertyName)
+        => entityTypeBuilder.TryConfigureConcurrencyStamp(_maxLength, propertyName);
+
+    public static void TryConfigureConcurrencyStamp(
+        this EntityTypeBuilder entityTypeBuilder,
+        int maxLength = _maxLength,
+        string? propertyName = null)
+    {
+        if (entityTypeBuilder.Metadata.ClrType.IsAssignableTo(typeof(IHasConcurrencyStamp)))
+        {
+            entityTypeBuilder.Property(nameof(IHasConcurrencyStamp.RowVersion))
+                .IsConcurrencyToken()
+                .HasMaxLength(maxLength)
+                .HasColumnName(propertyName ?? nameof(IHasConcurrencyStamp.RowVersion));
+        }
+    }
+}
