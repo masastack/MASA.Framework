e6436b2eb957a980f049e90cea3faf661246246a
wuweilai <824255785@qq.com>
2022-06-15 09:14:26 +0800

    refactor:refactor code

--- src/Authentication/Masa.Contrib.Authentication.Oidc.EntityFrameworkCore/Caches/SyncCache.cs
+++ src/Authentication/Masa.Contrib.Authentication.Oidc.EntityFrameworkCore/Caches/SyncCache.cs
@@ -1,8 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.Authentication.Oidc.EntityFrameworkCore.DbContexts;
-
 namespace Masa.Contrib.Authentication.Oidc.EntityFrameworkCore.Caches;
 
 public class SyncCache
--- src/Authentication/Masa.Contrib.Authentication.Oidc.EntityFrameworkCore/ServiceCollectionExtensions.cs
+++ src/Authentication/Masa.Contrib.Authentication.Oidc.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -1,10 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.Authentication.Oidc.EntityFrameworkCore.Caches;
-using Masa.Contrib.Authentication.Oidc.EntityFrameworkCore.DbContexts;
-using Masa.Contrib.Authentication.Oidc.EntityFrameworkCore.Repositories;
-
 namespace Masa.Contrib.Authentication.Oidc.EntityFrameworkCore;
 
 public static class ServiceCollectionExtensions
