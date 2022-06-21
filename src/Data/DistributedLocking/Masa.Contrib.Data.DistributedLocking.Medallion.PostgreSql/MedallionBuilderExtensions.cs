948d1ee7693f0df668bed1f53b0934b52e50c568
磊磊 <wangzhenlei520@gmail.com>
2022-06-21 11:47:40 +0800

    feat(distributedlock): Support distributed lock (#88)
    * feat: Support distributed lock
    
    * chore: Change the name of the method for acquiring distributed locks
    
    * feat(DistributedLocking): Support distributed lock

---
+++ src/Data/DistributedLocking/Masa.Contrib.Data.DistributedLocking.Medallion.PostgreSql/MedallionBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class MedallionBuilderExtensions
+{
+    public static void UseNpgsql(this MedallionBuilder medallionBuilder,
+        string connectionString,
+        Action<PostgresConnectionOptionsBuilder>? options = null)
+    {
+        medallionBuilder.Services.AddSingleton<IDistributedLockProvider>(_
+            => new PostgresDistributedSynchronizationProvider(connectionString, options));
+    }
+
+    public static void UseNpgsql<TDbContextType>(this MedallionBuilder medallionBuilder,
+        Action<PostgresConnectionOptionsBuilder>? options = null)
+    {
+        medallionBuilder.Services.AddSingleton<IDistributedLockProvider>(serviceProvider
+            =>
+        {
+            var unitOfWorkManager = serviceProvider.GetService<IUnitOfWorkManager>();
+            if (unitOfWorkManager == null)
+                throw new NotSupportedException("UoW is not supported");
+
+            using (var unitOfWork = unitOfWorkManager.CreateDbContext())
+            {
+                var name = ConnectionStringNameAttribute.GetConnStringName(typeof(TDbContextType));
+                var connectionStringProvider = unitOfWork.ServiceProvider.GetRequiredService<IConnectionStringProvider>();
+                var connectionString = connectionStringProvider.GetConnectionString(name);
+                return new PostgresDistributedSynchronizationProvider(connectionString, options);
+            }
+        });
+    }
+}
