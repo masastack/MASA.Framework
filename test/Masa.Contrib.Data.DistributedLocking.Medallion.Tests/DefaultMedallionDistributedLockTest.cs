cd385b8978ecc32e5893b5ab3ee043c828649868
zhenlei520 <wangzhenlei520@gmail.com>
2022-06-23 16:37:11 +0800

    chore: Medalion removes unit tests

--- test/Masa.Contrib.Data.DistributedLocking.Medallion.Tests/DefaultMedallionDistributedLockTest.cs
+++ test/Masa.Contrib.Data.DistributedLocking.Medallion.Tests/DefaultMedallionDistributedLockTest.cs
@@ -3,33 +3,36 @@
 
 namespace Masa.Contrib.Data.DistributedLocking.Medallion.Tests;
 
+/// <summary>
+/// Only supports local testing
+/// </summary>
 [TestClass]
 public class DefaultMedallionDistributedLockTest
 {
-    private IDistributedLock DistributedLock { get; set; }
-
-    [TestInitialize]
-    public void Initialize()
-    {
-        var services = new ServiceCollection();
-        services.AddDistributedLock(medallionBuilder =>
-        {
-            medallionBuilder.UseRedis("127.0.0.1:6379");
-        });
-        DistributedLock = services.GetInstance<IDistributedLock>();
-    }
-
-    [TestMethod]
-    public void TestDistributedLock()
-    {
-        using var obj = DistributedLock.TryGet("test", TimeSpan.FromSeconds(1));
-        Assert.IsNotNull(obj);
-    }
-
-    [TestMethod]
-    public async Task TestDistributedLockAsync()
-    {
-        await using var obj = await DistributedLock.TryGetAsync("test", TimeSpan.FromSeconds(1));
-        Assert.IsNotNull(obj);
-    }
+    // private IDistributedLock DistributedLock { get; set; }
+    //
+    // [TestInitialize]
+    // public void Initialize()
+    // {
+    //     var services = new ServiceCollection();
+    //     services.AddDistributedLock(medallionBuilder =>
+    //     {
+    //         medallionBuilder.UseRedis("127.0.0.1:6379");
+    //     });
+    //     DistributedLock = services.GetInstance<IDistributedLock>();
+    // }
+    //
+    // [TestMethod]
+    // public void TestDistributedLock()
+    // {
+    //     using var obj = DistributedLock.TryGet("test", TimeSpan.FromSeconds(1));
+    //     Assert.IsNotNull(obj);
+    // }
+    //
+    // [TestMethod]
+    // public async Task TestDistributedLockAsync()
+    // {
+    //     await using var obj = await DistributedLock.TryGetAsync("test", TimeSpan.FromSeconds(1));
+    //     Assert.IsNotNull(obj);
+    // }
 }
