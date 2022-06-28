aaa114774243b626f3b0814b8ac086c40f8b102f
Gavin <kevin_1ee@163.com>
2022-06-28 16:33:40 +0800

    chore: sort global using

--- src/BasicAbility/Masa.Contrib.BasicAbility.Scheduler/_Imports.cs
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Scheduler/_Imports.cs
@@ -2,14 +2,12 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.BasicAbility.Scheduler;
-global using Masa.Utils.Caller.Core;
+global using Masa.BuildingBlocks.BasicAbility.Scheduler.Request;
 global using Masa.BuildingBlocks.BasicAbility.Scheduler.Service;
 global using Masa.BuildingBlocks.Identity.IdentityModel;
-global using Masa.Contrib.BasicAbility.Scheduler.Services;
-global using Microsoft.Extensions.DependencyInjection;
-global using Masa.Utils.Caller.HttpClient;
-global using static Masa.Contrib.BasicAbility.Scheduler.Constants;
 global using Masa.Contrib.BasicAbility.Scheduler;
-global using Masa.BuildingBlocks.BasicAbility.Scheduler.Model;
-global using Masa.BuildingBlocks.BasicAbility.Scheduler.Request;
+global using Masa.Contrib.BasicAbility.Scheduler.Services;
+global using Masa.Utils.Caller.Core;
+global using Masa.Utils.Caller.HttpClient;
 global using Microsoft.Extensions.Logging;
+global using static Masa.Contrib.BasicAbility.Scheduler.Constants;
