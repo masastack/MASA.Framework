94b94c3b14b6895f6b5bfacf7eb5d2f215e7da84
Gavin <kevin_1ee@163.com>
2022-06-28 14:59:21 +0800

    feat: Scheduler SDK & SDK UnitTest

---
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Scheduler/Services/SchedulerTaskService.cs
@@ -0,0 +1,57 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.BasicAbility.Scheduler.Services;
+
+public class SchedulerTaskService : ISchedulerTaskService
+{
+    const string API = "/api/scheduler-task";
+
+    readonly ICallerProvider _callerProvider;
+    readonly ILogger<SchedulerTaskService> _logger;
+    readonly ILoggerFactory _loggerFactory;
+
+    public SchedulerTaskService(ICallerProvider callerProvider, ILoggerFactory loggerFactory)
+    {
+        _callerProvider = callerProvider;
+        _loggerFactory = loggerFactory;
+        _logger = _loggerFactory.CreateLogger<SchedulerTaskService>();
+    }
+
+    public async Task<bool> StopSchedulerTaskAsync(BaseSchedulerTaskRequest request)
+    {
+        try
+        {
+            var requestUri = $"{API}/stop";
+            await _callerProvider.PutAsync(requestUri, request);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "StopSchedulerTaskAsync Error");
+            return false;
+        }
+    }
+
+    public async Task<bool> StartSchedulerTaskAsync(BaseSchedulerTaskRequest request)
+    {
+        try
+        {
+            var requestData = new StartSchedulerTaskRequest()
+            {
+                TaskId = request.TaskId,
+                OperatorId = request.OperatorId,
+                IsManual = true
+            };
+
+            var requestUri = $"{API}/start";
+            await _callerProvider.PutAsync(requestUri, requestData);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "StopSchedulerTaskAsync Error");
+            return false;
+        }
+    }
+}
---
+++ test/Masa.Contrib.BasicAbility.Scheduler.Tests/_Imports.cs
@@ -0,0 +1,17 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+global using Masa.Utils.Caller.Core;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Moq;
+global using Masa.BuildingBlocks.BasicAbility.Scheduler.Request;
+global using System;
+global using Masa.BuildingBlocks.Identity.IdentityModel;
+global using Microsoft.Extensions.Logging;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.DependencyInjection;
+global using Masa.Utils.Caller.HttpClient;
+global using Masa.BuildingBlocks.BasicAbility.Scheduler;
+global using Masa.BuildingBlocks.BasicAbility.Scheduler.Model;
+global using Masa.BuildingBlocks.BasicAbility.Scheduler.Enum;
+global using System.Collections.Generic;
