f9b564b9ef0f343f5d2d424c5b54fc777026bf93
吴自浩 <wuzihao425@outlook.com>
2022-06-28 18:28:02 +0800

    feat(websiteMessage)：mc sdk

---
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Mc/Service/WebsiteMessageService.cs
@@ -0,0 +1,54 @@
+namespace Masa.Contrib.BasicAbility.Mc.Service;
+
+public class WebsiteMessageService : IWebsiteMessageService
+{
+    readonly ICallerProvider _callerProvider;
+    readonly string _party = "api/website-message";
+
+    public WebsiteMessageService(ICallerProvider callerProvider)
+    {
+        _callerProvider = callerProvider;
+    }
+
+    public async Task CheckAsync()
+    {
+        var requestUri = $"{_party}/Check";
+        await _callerProvider.PostAsync(requestUri, null);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var requestUri = $"{_party}/{id}";
+        await _callerProvider.DeleteAsync(requestUri, null);
+    }
+
+    public async Task<List<WebsiteMessageChannelModel>> GetChannelListAsync()
+    {
+        var requestUri = $"{_party}/GetChannelList";
+        return await _callerProvider.GetAsync<List<WebsiteMessageChannelModel>>(requestUri)??new();
+    }
+
+    public async Task<PaginatedList<WebsiteMessageModel>> GetListAsync(GetWebsiteMessageModel options)
+    {
+        var requestUri = $"{_party}";
+        return await _callerProvider.GetAsync<GetWebsiteMessageModel, PaginatedList<WebsiteMessageModel>>(requestUri, options) ?? new();
+    }
+
+    public async Task<List<WebsiteMessageModel>> GetNoticeListAsync(GetNoticeListModel options)
+    {
+        var requestUri = $"{_party}/GetNoticeList";
+        return await _callerProvider.GetAsync<GetNoticeListModel, List<WebsiteMessageModel>>(requestUri, options) ?? new();
+    }
+
+    public async Task ReadAsync(ReadWebsiteMessageModel options)
+    {
+        var requestUri = $"{_party}/Read";
+        await _callerProvider.PostAsync(requestUri, options);
+    }
+
+    public async Task SetAllReadAsync(ReadAllWebsiteMessageModel options)
+    {
+        var requestUri = $"{_party}/SetAllRead";
+        await _callerProvider.PostAsync(requestUri, options);
+    }
+}
---
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Mc/_Imports.cs
@@ -0,0 +1,7 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+global using Masa.BuildingBlocks.BasicAbility.Mc.Service;
+global using Masa.Utils.Caller.Core;
+global using Masa.BuildingBlocks.BasicAbility.Mc.Model;
+global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
