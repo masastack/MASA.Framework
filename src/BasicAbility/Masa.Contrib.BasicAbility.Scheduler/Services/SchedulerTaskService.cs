fe73b7837618efa6e6aaf91bfaf9ad8616a0bd11
Gavin Li <54987555+gavin1ee@users.noreply.github.com>
2022-06-29 09:46:31 +0800

    feat: Scheduler SDK & SDK UnitTest (#102)
    * feat: Scheduler SDK & SDK UnitTest
    
    * chore: sort global using
    
    * rename: remove meaningless repeat function name
    
    * resolve conversation
    
    * resolve conversation
    
    * chore: resolve merge error

---
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Scheduler/Services/SchedulerTaskService.cs
@@ -0,0 +1,55 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.BasicAbility.Scheduler.Services;
+
+public class SchedulerTaskService : ISchedulerTaskService
+{
+    const string API = "/api/scheduler-task";
+
+    readonly ICallerProvider _callerProvider;
+    readonly ILogger<SchedulerTaskService>? _logger;
+
+    public SchedulerTaskService(ICallerProvider callerProvider, ILoggerFactory? loggerFactory)
+    {
+        _callerProvider = callerProvider;
+        _logger = loggerFactory?.CreateLogger<SchedulerTaskService>();
+    }
+
+    public async Task<bool> StopAsync(BaseSchedulerTaskRequest request)
+    {
+        try
+        {
+            var requestUri = $"{API}/stop";
+            await _callerProvider.PutAsync(requestUri, request);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger?.LogError(ex, "StopSchedulerTaskAsync Error");
+            return false;
+        }
+    }
+
+    public async Task<bool> StartAsync(BaseSchedulerTaskRequest request)
+    {
+        try
+        {
+            var requestData = new StartSchedulerTaskRequest()
+            {
+                TaskId = request.TaskId,
+                OperatorId = request.OperatorId,
+                IsManual = true
+            };
+
+            var requestUri = $"{API}/start";
+            await _callerProvider.PutAsync(requestUri, requestData);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger?.LogError(ex, "StopSchedulerTaskAsync Error");
+            return false;
+        }
+    }
+}
---
+++ test/Masa.Contrib.BasicAbility.Scheduler.Tests/SchedulerTaskServiceTest.cs
@@ -0,0 +1,48 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.BasicAbility.Scheduler.Tests;
+
+[TestClass]
+public class SchedulerTaskServiceTest
+{
+    const string API = "/api/scheduler-task";
+
+    [TestMethod]
+    public async Task TestStopSchedulerTaskAsync()
+    {
+        var requestData = new BaseSchedulerTaskRequest()
+        {
+            TaskId = Guid.NewGuid(),
+            OperatorId = Guid.NewGuid()
+        };
+
+        var requestUri = $"{API}/stop";
+        var callerProvider = new Mock<ICallerProvider>();
+        var loggerFactory = new Mock<ILoggerFactory>();
+        callerProvider.Setup(provider => provider.PutAsync<BaseSchedulerTaskRequest>(requestUri, requestData, true, default)).Verifiable();
+        var schedulerClient = new SchedulerClient(callerProvider.Object, loggerFactory.Object);
+        var result = await schedulerClient.SchedulerTaskService.StopAsync(requestData);
+        callerProvider.Verify(provider => provider.PutAsync<BaseSchedulerTaskRequest>(requestUri, requestData, true, default), Times.Once);
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public async Task TestStartSchedulerTaskAsync()
+    {
+        var request = new BaseSchedulerTaskRequest()
+        {
+            TaskId = Guid.NewGuid(),
+            OperatorId = Guid.NewGuid()
+        };
+
+        var requestUri = $"{API}/start";
+        var callerProvider = new Mock<ICallerProvider>();
+        var loggerFactory = new Mock<ILoggerFactory>();
+        callerProvider.Setup(provider => provider.PutAsync(requestUri, It.IsAny<StartSchedulerTaskRequest>(), true, default)).Verifiable();
+        var schedulerClient = new SchedulerClient(callerProvider.Object, loggerFactory.Object);
+        var result = await schedulerClient.SchedulerTaskService.StartAsync(request);
+        callerProvider.Verify(provider => provider.PutAsync(requestUri, It.IsAny<StartSchedulerTaskRequest>(), true, default), Times.Once);
+        Assert.IsTrue(result);
+    }
+}
