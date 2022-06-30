c403887c00bffe04e1b6ed13d749adeb33dd6431
吴自浩 <wuzihao425@outlook.com>
2022-06-30 10:32:31 +0800

    refactor：process code review

--- src/BasicAbility/Masa.Contrib.BasicAbility.Mc/_Imports.cs
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Mc/_Imports.cs
@@ -1,13 +1,16 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-global using Masa.BuildingBlocks.BasicAbility.Mc.Service;
-global using Masa.Utils.Caller.Core;
-global using Masa.BuildingBlocks.BasicAbility.Mc.Model;
-global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
 global using Masa.BuildingBlocks.BasicAbility.Mc;
-global using Masa.Contrib.BasicAbility.Mc.Service;
-global using Masa.Utils.Caller.HttpClient;
-global using Microsoft.Extensions.DependencyInjection;
-global using static Masa.Contrib.BasicAbility.Mc.Constants;
+global using Masa.BuildingBlocks.BasicAbility.Mc.Service;
+global using Masa.BuildingBlocks.BasicAbility.Mc.Model;
 global using Masa.Contrib.BasicAbility.Mc;
+global using static Masa.Contrib.BasicAbility.Mc.Constants;
+global using Masa.Contrib.BasicAbility.Mc.Infrastructure.Helper;
+global using Masa.Contrib.BasicAbility.Mc.Service;
+global using Masa.Utils.Caller.Core;
+global using Masa.Utils.Caller.HttpClient;
+global using System.ComponentModel;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
+global using System.Reflection;
