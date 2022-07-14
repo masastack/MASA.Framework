a98c46c53a3d0e3caaea62cb0c954dbacb235e4e
wzh425 <wuzihao425@outlook.com>
2022-07-14 14:40:59 +0800

    refactor：reduction

--- src/BasicAbility/Masa.Contrib.BasicAbility.Mc/McClient.cs
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Mc/McClient.cs
@@ -12,7 +12,6 @@ public class McClient: IMcClient
         MessageTemplateService = new MessageTemplateService(callerProvider);
         ReceiverGroupService = new ReceiverGroupService(callerProvider);
         WebsiteMessageService = new WebsiteMessageService(callerProvider);
-        NotificationService = new NotificationService(callerProvider);
     }
 
     public IChannelService ChannelService { get; }
@@ -24,6 +23,4 @@ public class McClient: IMcClient
     public IReceiverGroupService ReceiverGroupService { get; }
 
     public IWebsiteMessageService WebsiteMessageService { get; }
-
-    public INotificationService NotificationService { get; }
 }
--- src/BasicAbility/Masa.Contrib.BasicAbility.Mc/_Imports.cs
+++ src/BasicAbility/Masa.Contrib.BasicAbility.Mc/_Imports.cs
@@ -14,3 +14,5 @@ global using System.ComponentModel;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.Reflection;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Authentication;
