d96fcbfbfb229fb19f37e187af58381ee87c5e27
磊磊 <wangzhenlei520@gmail.com>
2022-07-18 17:20:09 +0800

    feat(EntityFramework): Support migration of local message table via IDesignTimeDbContextFactory<TContext> (#152)
    * feat(EntityFramework): Support migration of local message table via IDesignTimeDbContextFactory<TContext>
    
    * doc: Modify the use of cross-process events documentation
    
    * feat(EntityFramework): Support migration of local message table via IDesignTimeDbContextFactory<TContext>

--- src/Dispatcher/Masa.Contrib.Dispatcher.IntegrationEvents.EventLogs.EF/IntegrationEventLogModelCreatingProvider.cs
+++ src/Dispatcher/Masa.Contrib.Dispatcher.IntegrationEvents.EventLogs.EF/IntegrationEventLogModelCreatingProvider.cs
@@ -7,43 +7,7 @@ public class IntegrationEventLogModelCreatingProvider : IModelCreatingProvider
 {
     public void Configure(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<IntegrationEventLog>(ConfigureEventLogEntry);
-    }
-
-    private void ConfigureEventLogEntry(EntityTypeBuilder<IntegrationEventLog> builder)
-    {
-        builder.ToTable("IntegrationEventLog");
-
-        builder.HasKey(e => e.Id);
-
-        builder.Property(e => e.Id)
-            .IsRequired();
-
-        builder.Property(e => e.Content)
-            .IsRequired();
-
-        builder.Property(e => e.CreationTime)
-            .IsRequired();
-
-        builder.Property(e => e.ModificationTime)
-            .IsRequired();
-
-        builder.Property(e => e.State)
-            .IsRequired();
-
-        builder.Property(e => e.TimesSent)
-            .IsRequired();
-
-        builder.Property(nameof(IHasConcurrencyStamp.RowVersion))
-            .IsConcurrencyToken()
-            .HasMaxLength(36)
-            .HasColumnName(nameof(IHasConcurrencyStamp.RowVersion));
-
-        builder.Property(e => e.EventTypeName)
-            .IsRequired();
-
-        builder.HasIndex(e => new { e.State, e.ModificationTime }, "index_state_modificationtime");
-        builder.HasIndex(e => new { e.State, e.TimesSent, e.ModificationTime }, "index_state_timessent_modificationtime");
-        builder.HasIndex(e => new { e.EventId, e.RowVersion }, "index_eventid_version");
+        var integrationEventLogEntityTypeConfiguration = new IntegrationEventLogEntityTypeConfiguration();
+        modelBuilder.Entity<IntegrationEventLog>(integrationEventLogEntityTypeConfiguration.Configure);
     }
 }
