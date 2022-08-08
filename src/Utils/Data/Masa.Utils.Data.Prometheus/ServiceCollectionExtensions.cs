99bb1cedc1635ebc342b7536fc2e373f1208e774
qinyouzeng <qinyouzeng@masastack.com>
2022-08-08 17:26:16 +0800

    feat: replace caller with httpclient

--- src/Utils/Data/Masa.Utils.Data.Prometheus/ServiceCollectionExtensions.cs
+++ src/Utils/Data/Masa.Utils.Data.Prometheus/ServiceCollectionExtensions.cs
@@ -5,23 +5,14 @@ namespace Masa.Utils.Data.Prometheus;
 
 public static class ServiceCollectionExtensions
 {
-    private const string PROMETHEUS_HTTP_CLIENT_NAME = "prometheus_client_name";
+    private const string PROMETHEUS_HTTP_CLIENT_NAME = "masa_stack_prometheus_client";
 
-    public static IServiceCollection AddPrometheusClient(this IServiceCollection services, string url)
+    public static IServiceCollection AddPrometheusClient(this IServiceCollection services, string url, int timeoutSeconds = 5)
     {
         ArgumentNullException.ThrowIfNull(url, nameof(url));
 
         if (services.Any(service => service.GetType() == typeof(IMasaPrometheusClient)))
             return services;
-
-        services.AddCaller(builder =>
-        {
-            builder.UseHttpClient(options =>
-            {
-                options.BaseAddress = url;
-                options.Name = PROMETHEUS_HTTP_CLIENT_NAME;
-            });
-        });
 
         var jsonOptions = new JsonSerializerOptions
         {
@@ -29,10 +20,18 @@ public static class ServiceCollectionExtensions
         };
         jsonOptions.Converters.Add(new JsonStringEnumConverter());
 
+        if (timeoutSeconds <= 0)
+            timeoutSeconds = 5;
+        services.AddHttpClient(PROMETHEUS_HTTP_CLIENT_NAME, ops =>
+        {
+            ops.BaseAddress = new Uri(url);
+            ops.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+        });
+
         services.AddScoped<IMasaPrometheusClient>(ServiceProvider =>
         {
-            var caller = ServiceProvider.GetRequiredService<ICallerFactory>().CreateClient(PROMETHEUS_HTTP_CLIENT_NAME);
-            return new MasaPrometheusClient(caller, jsonOptions);
+            var client = ServiceProvider.GetRequiredService<IHttpClientFactory>().CreateClient(PROMETHEUS_HTTP_CLIENT_NAME);
+            return new MasaPrometheusClient(client, jsonOptions, ServiceProvider.GetRequiredService<ILogger<MasaPrometheusClient>>());
         });
         return services;
     }
