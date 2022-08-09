ad104a6d1a8f34a4fc9c7738f18d0f89e2974048
Qinyouzeng <102203523+Qinyouzeng@users.noreply.github.com>
2022-08-09 10:37:43 +0800

    chore: tests update (#187)

--- src/Utils/Data/Tests/Masa.Utils.Data.Prometheus.Test/_Imports.cs
+++ src/Utils/Data/Tests/Masa.Utils.Data.Prometheus.Test/_Imports.cs
@@ -4,9 +4,17 @@
 global using Masa.Utils.Data.Prometheus.Enums;
 global using Masa.Utils.Data.Prometheus.Model;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Moq;
+global using Moq.Protected;
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
+global using System.Net;
+global using System.Net.Http;
 global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Threading;
 global using System.Threading.Tasks;
