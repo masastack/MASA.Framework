9d00a8846dae5a7b381bbffe3437d02acbb396d8
zhenlei520 <wangzhenlei520@gmail.com>
2022-09-02 15:14:21 +0800

    chore: global using sort

--- src/Contrib/Caching/Distributed/Masa.Contrib.Caching.Distributed.StackExchangeRedis/_Imports.cs
+++ src/Contrib/Caching/Distributed/Masa.Contrib.Caching.Distributed.StackExchangeRedis/_Imports.cs
@@ -2,19 +2,15 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Caching;
-global using Masa.BuildingBlocks.Configuration;
-global using Masa.Contrib.Caching.Distributed;
 global using Masa.Contrib.Caching.Distributed.StackExchangeRedis;
-global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Options;
 global using StackExchange.Redis;
 global using System.Collections;
 global using System.ComponentModel;
+global using System.Diagnostics;
 global using System.Dynamic;
 global using System.IO.Compression;
 global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
-global using System.Diagnostics;
--- src/Contrib/Caching/Distributed/Tests/Masa.Contrib.Caching.Distributed.StackExchangeRedis.Tests/_Imports.cs
+++ src/Contrib/Caching/Distributed/Tests/Masa.Contrib.Caching.Distributed.StackExchangeRedis.Tests/_Imports.cs
@@ -1,9 +1,8 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
+// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Caching;
 global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using StackExchange.Redis;
