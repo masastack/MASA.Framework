3b691b5c495208f40d09e87d9a2bae20c391aa0e
zhenlei520 <wangzhenlei520@gmail.com>
2022-09-05 18:12:19 +0800

    chore: global using sort

--- src/Contrib/Caching/Masa.Contrib.Caching.MultilevelCache.Tests/_Imports.cs
+++ src/Contrib/Caching/Masa.Contrib.Caching.MultilevelCache.Tests/_Imports.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Caching;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.Caching.Memory;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using NSubstitute;
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.Options;
-global using Microsoft.Extensions.Caching.Memory;
