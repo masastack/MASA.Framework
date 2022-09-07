d827cdee07eadb3efc0b11c6263dbca57adc0285
Mayue <mayue@lonsid.cn>
2022-09-07 17:18:41 +0800

    refactor:global using

--- src/BuildingBlocks/StackSdks/Auth/Masa.BuildingBlocks.StackSdks.Auth.Contracts/MasaUser.cs
+++ src/BuildingBlocks/StackSdks/Auth/Masa.BuildingBlocks.StackSdks.Auth.Contracts/MasaUser.cs
@@ -1,8 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.BuildingBlocks.Authentication.Identity;
-
 namespace Masa.BuildingBlocks.StackSdks.Auth.Contracts;
 
 public class MasaUser : IIdentityUser
