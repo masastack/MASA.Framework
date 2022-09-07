48ecb5818cc8bd4abfdceb4adf7459b4b4f9b69b
wuweilai <30889371+15168440402@users.noreply.github.com>
2022-09-07 12:14:18 +0800

    Feature( user)add third party user、login for phone number、remove user roles (#231)
    * feature(thirdPartyUser):add third party user
    
    * refactor:refactor code
    
    * feat(user):add login for phoneNumber、remove user roles
    
    * refactor:refactor code
    
    * refactor:refactor code
    
    * refactor:refactor code

---
+++ src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Auth.Tests/CustomLoginServiceTest.cs
@@ -0,0 +1,21 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.StackSdks.Auth.Tests;
+
+[TestClass]
+public class CustomLoginServiceTest
+{
+    [TestMethod]
+    public async Task TestGetCustomLoginByClientIdAsync()
+    {
+        var data = new CustomLoginModel();
+        string clientId = Guid.NewGuid().ToString();
+        var requestUri = $"api/sso/customLogin/getCustomLoginByClientId";
+        var caller = new Mock<ICaller>();
+        caller.Setup(provider => provider.GetAsync<object, CustomLoginModel>(requestUri, It.IsAny<object>(), default)).ReturnsAsync(data).Verifiable();
+        var customLoginService = new CustomLoginService(caller.Object);
+        var result = await customLoginService.GetCustomLoginByClientIdAsync(clientId);
+        caller.Verify(provider => provider.GetAsync<object, CustomLoginModel>(requestUri, It.IsAny<object>(), default), Times.Once);
+    }
+}
