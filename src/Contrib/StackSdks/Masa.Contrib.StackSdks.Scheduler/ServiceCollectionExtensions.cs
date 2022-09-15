242b51c9b852af574c2fa617de31dafee4923957
磊磊 <wangzhenlei520@gmail.com>
2022-09-15 09:43:47 +0800

    feat(Caller): Add DisableAutoRegistration (#240)
    * feat(Caller): Add DisableAutoRegistration
    
    * chore(Caller): remove warning

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Scheduler/ServiceCollectionExtensions.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Scheduler/ServiceCollectionExtensions.cs
@@ -14,7 +14,7 @@ public static class ServiceCollectionExtensions
 
         return services.AddSchedulerClient(callerOptions =>
         {
-            callerOptions.Assemblies = Array.Empty<Assembly>();
+            callerOptions.DisableAutoRegistration = true;
             callerOptions.UseHttpClient(DEFAULT_CLIENT_NAME, builder =>
             {
                 builder.Configure = opt => opt.BaseAddress = new Uri(schedulerServiceBaseAddress);
