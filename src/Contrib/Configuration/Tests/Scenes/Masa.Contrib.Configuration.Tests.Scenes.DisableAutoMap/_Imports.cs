e287e4d93ea11e0a0fffe03d95a99bea2408b42d
zhenlei520 <wangzhenlei520@gmail.com>
2022-09-22 17:01:32 +0800

    test(Configuration): add UnitTest

--- src/Contrib/Configuration/Tests/Scenes/Masa.Contrib.Configuration.Tests.Scenes.DisableAutoMap/_Imports.cs
+++ src/Contrib/Configuration/Tests/Scenes/Masa.Contrib.Configuration.Tests.Scenes.DisableAutoMap/_Imports.cs
@@ -1,11 +1,7 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-global using Masa.BuildingBlocks.Configuration;
 global using Masa.BuildingBlocks.Configuration.Options;
-global using Masa.Contrib.Configuration.Tests.Config;
-global using Masa.Contrib.Configuration.Tests.Scenes.AutoMap;
-global using Masa.Contrib.Configuration.Tests.Scenes.NoLessConstructor;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
