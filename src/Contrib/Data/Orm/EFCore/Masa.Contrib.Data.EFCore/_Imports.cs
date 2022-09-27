eb183219faf1d20a6e20225a5a84c755d626ea0b
zhenlei520 <wangzhenlei520@gmail.com>
2022-09-27 17:48:48 +0800

    chore: Add global using

--- src/Contrib/Data/Orm/EFCore/Masa.Contrib.Data.EFCore/_Imports.cs
+++ src/Contrib/Data/Orm/EFCore/Masa.Contrib.Data.EFCore/_Imports.cs
@@ -2,16 +2,15 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Authentication.Identity;
-global using Masa.BuildingBlocks.Configuration;
 global using Masa.BuildingBlocks.Data;
 global using Masa.BuildingBlocks.Ddd.Domain.Entities;
 global using Masa.BuildingBlocks.Ddd.Domain.Entities.Auditing;
 global using Masa.BuildingBlocks.Ddd.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.ChangeTracking;
 global using Microsoft.EntityFrameworkCore.Infrastructure;
 global using Microsoft.EntityFrameworkCore.Metadata;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Options;
