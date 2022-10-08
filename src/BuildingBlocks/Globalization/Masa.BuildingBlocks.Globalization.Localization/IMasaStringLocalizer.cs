ead4e9404ede7c654c0e56e23d8c26f5faacc96c
zhenlei520 <wangzhenlei520@gmail.com>
2022-10-08 18:07:23 +0800

    feat: Add Globalization

---
+++ src/BuildingBlocks/Globalization/Masa.BuildingBlocks.Globalization.Localization/IMasaStringLocalizer.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Microsoft.Extensions.Localization;
+
+public interface IMasaStringLocalizer
+{
+    /// <summary>Gets the string resource with the given name.</summary>
+    /// <param name="name">The name of the string resource.</param>
+    /// <returns>The string resource as a <see cref="T:Microsoft.Extensions.Localization.LocalizedString" />.</returns>
+    LocalizedString this[string name] { get; }
+
+    /// <summary>
+    /// Gets the string resource with the given name and formatted with the supplied arguments.
+    /// </summary>
+    /// <param name="name">The name of the string resource.</param>
+    /// <param name="arguments">The values to format the string with.</param>
+    /// <returns>The formatted string resource as a <see cref="T:Microsoft.Extensions.Localization.LocalizedString" />.</returns>
+    LocalizedString this[string name, params object[] arguments] { get; }
+
+    /// <summary>Gets all string resources.</summary>
+    /// <param name="includeParentCultures">
+    /// A <see cref="T:System.Boolean" /> indicating whether to include strings from parent cultures.
+    /// </param>
+    /// <returns>The strings.</returns>
+    IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures);
+}
---
+++ src/BuildingBlocks/Globalization/Masa.BuildingBlocks.Globalization.Localization/Options/MasaLocalizationOptions.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Microsoft.Extensions.Localization;
+
+public class MasaLocalizationOptions
+{
+    public Type DefaultResourceType { get; set; }
+
+    public List<LanguageInfo> Languages { get; set; }
+}
