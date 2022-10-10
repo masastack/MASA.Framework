7040e42251c243a3992ff811a6624a76549910d7
zhenlei520 <wangzhenlei520@gmail.com>
2022-10-10 09:04:03 +0800

    chore: global using sort

--- src/Contrib/Service/Caller/Authentication/Masa.Contrib.Service.Caller.Authentication.OpenIdConnect/_Imports.cs
+++ src/Contrib/Service/Caller/Authentication/Masa.Contrib.Service.Caller.Authentication.OpenIdConnect/_Imports.cs
@@ -1,16 +1,16 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-global using Masa.BuildingBlocks.Service.Caller;
-global using System.Net.Http.Headers;
-global using System.IdentityModel.Tokens.Jwt;
-global using System.Security.Cryptography;
 global using IdentityModel;
 global using IdentityModel.Client;
+global using Masa.BuildingBlocks.Service.Caller;
+global using Masa.Contrib.Service.Caller.Authentication.OpenIdConnect;
+global using Masa.Contrib.Service.Caller.Authentication.OpenIdConnect.Jwt;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using Microsoft.IdentityModel.Tokens;
-global using Masa.Contrib.Service.Caller.Authentication.OpenIdConnect;
-global using Microsoft.Extensions.DependencyInjection;
-global using Masa.Contrib.Service.Caller.Authentication.OpenIdConnect.Jwt;
-global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Net.Http.Headers;
+global using System.Security.Cryptography;
--- src/Contrib/Service/Caller/Authentication/Tests/Masa.Contrib.Service.Caller.Authentication.OpenIdConnect.Tests/_Imports.cs
+++ src/Contrib/Service/Caller/Authentication/Tests/Masa.Contrib.Service.Caller.Authentication.OpenIdConnect.Tests/_Imports.cs
@@ -5,7 +5,3 @@ global using Masa.BuildingBlocks.Service.Caller;
 global using Masa.BuildingBlocks.Service.Caller.Options;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Masa.Contrib.Service.Caller.Authentication.OpenIdConnect.Jwt;
-global using System.Net;
-global using Moq;
-global using Moq.Protected;
