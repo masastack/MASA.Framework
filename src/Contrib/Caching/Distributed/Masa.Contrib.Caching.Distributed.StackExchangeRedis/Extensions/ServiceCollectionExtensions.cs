0cba01cb739fa61424f67280fc3783082c1bcce8
zhenlei520 <wangzhenlei520@gmail.com>
2022-10-19 13:02:36 +0800

    chore: remove extra spaces

--- src/Contrib/Caching/Distributed/Masa.Contrib.Caching.Distributed.StackExchangeRedis/Extensions/ServiceCollectionExtensions.cs
+++ src/Contrib/Caching/Distributed/Masa.Contrib.Caching.Distributed.StackExchangeRedis/Extensions/ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 // ReSharper disable once CheckNamespace
-
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
