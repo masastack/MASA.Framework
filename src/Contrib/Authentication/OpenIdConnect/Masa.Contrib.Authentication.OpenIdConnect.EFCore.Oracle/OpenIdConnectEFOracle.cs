4f7b6cfc8b6f9f5860890511ecbad592991e3316
Hx <120253707@QQ.com>
2022-10-21 16:02:02 +0800

    feat: add openidConnect oracle assembly

---
+++ src/Contrib/Authentication/OpenIdConnect/Masa.Contrib.Authentication.OpenIdConnect.EFCore.Oracle/OpenIdConnectEFOracle.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+
+namespace Masa.Contrib.Authentication.OpenIdConnect.EFCore.Oracle
+{
+    public class OpenIdConnectEFOracle
+    {
+        public static Assembly Assembly => typeof(OpenIdConnectEFOracle).Assembly;
+    }
+}
+
