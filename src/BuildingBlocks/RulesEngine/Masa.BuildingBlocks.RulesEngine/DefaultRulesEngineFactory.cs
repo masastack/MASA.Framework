be8e38d2b0ac55efbe6a38a6a6cce3ef790a1fc8
磊磊 <wangzhenlei520@gmail.com>
2022-10-26 17:21:05 +0800

    feat(RulesEngine): Add RulesEngine (#311)
    * feat(RulesEngine): Add RulesEngine
    
    * feat(RulesEngine): Add RulesEngine
    
    * chore: remove redundant references
    
    * chore: fix bad code smell

---
+++ src/BuildingBlocks/RulesEngine/Masa.BuildingBlocks.RulesEngine/DefaultRulesEngineFactory.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.RulesEngine;
+
+public class DefaultRulesEngineFactory : MasaFactoryBase<IRulesEngineClient, RulesEngineRelationOptions>,
+    IRulesEngineFactory
+{
+    protected override string DefaultServiceNotFoundMessage
+        => $"No default {nameof(IRulesEngineClient)} found, you may need services.AddRulesEngine(options => options.UseRulesEngine())";
+
+    protected override string SpecifyServiceNotFoundMessage
+        => $"Please make sure you have used [{0}] {nameof(IRulesEngineClient)}, it was not found";
+
+    protected override MasaFactoryOptions<RulesEngineRelationOptions> FactoryOptions => _options.Value;
+
+    private readonly IOptions<RulesEngineFactoryOptions> _options;
+
+    public DefaultRulesEngineFactory(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+        _options = serviceProvider.GetRequiredService<IOptions<RulesEngineFactoryOptions>>();
+    }
+}
