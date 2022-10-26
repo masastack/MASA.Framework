372871cd27d028c1e9603f71c782723cf379c440
鬼谷子 <358683537@qq.com>
2022-10-26 12:50:42 +0800

    Revert "feat(RulesEngine): Add RulesEngine (#308)" (#310)
    This reverts commit d50b1facbcb9b31a027fafe5269db2ceeb277a96.

--- src/BuildingBlocks/Data/Masa.BuildingBlocks.Data.Contracts/Options/MasaRelationOptions.cs
+++ src/BuildingBlocks/Data/Masa.BuildingBlocks.Data.Contracts/Options/MasaRelationOptions.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-// ReSharper disable once CheckNamespace
-
 namespace Masa.BuildingBlocks.Data;
 
 public class MasaRelationOptions
