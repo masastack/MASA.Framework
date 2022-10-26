4ebe933620cf890b82139039a5c788e3b33e8db6
鬼谷子 <358683537@qq.com>
2022-10-26 12:50:10 +0800

    Revert "feat(RulesEngine): Add RulesEngine (#308)"
    This reverts commit d50b1facbcb9b31a027fafe5269db2ceeb277a96.

--- src/Contrib/RulesEngine/Tests/Masa.Contrib.RulesEngine.MicrosoftRulesEngine.Tests/_Imports.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-global using Masa.BuildingBlocks.RulesEngine;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Logging;
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using RulesEngine.Models;
