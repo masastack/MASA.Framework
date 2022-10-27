d3218fcfad326e1698d37bb6b47ae2a20ece4bbe
zhenlei520 <wangzhenlei520@gmail.com>
2022-10-27 17:30:03 +0800

    chore: global using sort

--- src/Contrib/Development/Masa.Contrib.Development.DaprStarters.AspNetCore/_Imports.cs
+++ src/Contrib/Development/Masa.Contrib.Development.DaprStarters.AspNetCore/_Imports.cs
@@ -1,12 +1,11 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+global using Masa.BuildingBlocks.Development.DaprStarters;
+global using Masa.Contrib.Development.DaprStarters.AspNetCore;
 global using Microsoft.AspNetCore.Hosting.Server;
 global using Microsoft.AspNetCore.Hosting.Server.Features;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
-global using Masa.BuildingBlocks.Development.DaprStarters;
-global using Masa.Contrib.Development.DaprStarters.AspNetCore;
-global using Microsoft.Extensions.DependencyInjection;
