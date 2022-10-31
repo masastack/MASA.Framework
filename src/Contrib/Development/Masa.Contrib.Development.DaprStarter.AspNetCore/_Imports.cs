e0f07f3c9350a1cc2c9df7e60469ce0a889db623
磊磊 <wangzhenlei520@gmail.com>
2022-10-31 14:28:58 +0800

    feat(dapr starter): Add Dapr Starter (#312)
    * feat(DaprStarter): Add DaprStarter
    
    * feat(DaprStarter): Add DaprStarter
    
    * feat(DaprStarter): Caller supports  DaprStarter
    
    * feature(DaprStarter): Add Readme
    
    * chore: global using sort
    
    * test(DaprStarter): Add DaprStarter
    
    * test(DaprStarter): Add Masa.Contrib.Development.DaprStarters.AspNetCore.Tests
    
    * test(DaprStarter): Add DaprStarter
    
    * fix(DaprStarter): Fix Refresh method
    
    * chore: DaprStarters -> DaprStarter

--- src/Contrib/Development/Masa.Contrib.Development.DaprStarter.AspNetCore/_Imports.cs
+++ src/Contrib/Development/Masa.Contrib.Development.DaprStarter.AspNetCore/_Imports.cs
@@ -1,13 +1,13 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-global using Dapr.Client;
 global using Masa.BuildingBlocks.Development.DaprStarter;
-global using Masa.BuildingBlocks.Service.Caller;
-global using Masa.BuildingBlocks.Service.Caller.Options;
-global using Masa.Contrib.Service.Caller.DaprClient.Options;
-global using Microsoft.Extensions.DependencyInjection;
+global using Masa.Contrib.Development.DaprStarter.AspNetCore;
+global using Microsoft.AspNetCore.Hosting.Server;
+global using Microsoft.AspNetCore.Hosting.Server.Features;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
-global using System.Net.NetworkInformation;
+global using System.Diagnostics.CodeAnalysis;
---
+++ src/Contrib/Development/Masa.Contrib.Development.DaprStarter/Process/IProcessProvider.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+// ReSharper disable once CheckNamespace
+
+namespace Masa.Contrib.Development.DaprStarter;
+
+public interface IProcessProvider
+{
+    IProcess GetProcess(int pId);
+
+    /// <summary>
+    /// Is the port available
+    /// </summary>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    bool IsAvailablePorts(ushort port);
+
+    List<int> GetPidByPort(ushort port);
+}
