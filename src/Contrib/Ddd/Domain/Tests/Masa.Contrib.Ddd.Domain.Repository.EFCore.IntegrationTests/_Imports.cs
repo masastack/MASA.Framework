c299291d20b3952df79feb3014819bf20b3051eb
磊磊 <wangzhenlei520@gmail.com>
2022-11-28 15:13:41 +0800

    Value objects support nesting (#347)
    * refactor(Orm): Masa.Contrib.Data.Contracts.EFCore -> Masa.Contrib.Data.Contracts
    
    * feature(Orm): value object nesting
    
    * chore: global using sort
    
    * feature(EFCore): Modify soft delete conditions
    
    * chore: Remove the blank line.

---
+++ src/Contrib/Ddd/Domain/Tests/Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests/_Imports.cs
@@ -0,0 +1,19 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+global using Masa.BuildingBlocks.Data;
+global using Masa.BuildingBlocks.Ddd.Domain.Entities;
+global using Masa.BuildingBlocks.Ddd.Domain.Entities.Auditing;
+global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
+global using Masa.BuildingBlocks.Ddd.Domain.Values;
+global using Masa.BuildingBlocks.Dispatcher.Events;
+global using Masa.Contrib.Data.UoW.EFCore;
+global using Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests.Domain.Entities;
+global using Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests.EntityTypeConfigurations;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using System;
+global using System.Collections.Generic;
+global using System.Reflection;
