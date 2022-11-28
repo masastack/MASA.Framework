aff170bd4983f216eef954a1351a33a6c6380c70
zhenlei520 <wangzhenlei520@gmail.com>
2022-11-28 10:31:36 +0800

    chore: global using sort

--- src/Contrib/Ddd/Domain/Tests/Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests/_Imports.cs
+++ src/Contrib/Ddd/Domain/Tests/Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests/_Imports.cs
@@ -2,21 +2,18 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Data;
-global using Masa.BuildingBlocks.Data.UoW;
 global using Masa.BuildingBlocks.Ddd.Domain.Entities;
 global using Masa.BuildingBlocks.Ddd.Domain.Entities.Auditing;
+global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
 global using Masa.BuildingBlocks.Ddd.Domain.Values;
+global using Masa.BuildingBlocks.Dispatcher.Events;
+global using Masa.Contrib.Data.UoW.EFCore;
 global using Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests.Domain.Entities;
+global using Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests.EntityTypeConfigurations;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Moq;
 global using System;
 global using System.Collections.Generic;
-global using EntityState = Masa.BuildingBlocks.Data.UoW.EntityState;
 global using System.Reflection;
-global using Masa.BuildingBlocks.Dispatcher.Events;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Masa.Contrib.Ddd.Domain.Repository.EFCore.IntegrationTests.EntityTypeConfigurations;
-global using Masa.BuildingBlocks.Ddd.Domain.Repositories;
-global using Masa.Contrib.Data.UoW.EFCore;
