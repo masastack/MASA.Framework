f9d10fc95e9a55366823abd062c300ee9d8edb5c
zhenlei520 <wangzhenlei520@gmail.com>
2022-12-07 11:18:50 +0800

    chore: Remove invalid namespace references

--- src/Utils/Data/Masa.Utils.Data.Elasticsearch/IElasticClientFactory.cs
+++ src/Utils/Data/Masa.Utils.Data.Elasticsearch/IElasticClientFactory.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Microsoft.Extensions.Options;
-
 namespace Masa.Utils.Data.Elasticsearch;
 
 public interface IElasticClientFactory
