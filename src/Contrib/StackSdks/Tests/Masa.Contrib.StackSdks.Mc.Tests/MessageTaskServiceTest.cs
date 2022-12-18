74a647b194b940f6188234791bf68fdb45a246b3
wzh425 <wuzihao425@outlook.com>
2022-12-18 20:26:39 +0800

    test : add unit testing

--- src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Mc.Tests/MessageTaskServiceTest.cs
+++ src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Mc.Tests/MessageTaskServiceTest.cs
@@ -1,8 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.Contrib.StackSdks.Mc.Service;
-
 namespace Masa.Contrib.StackSdks.Mc.Tests;
 
 [TestClass]
@@ -68,5 +66,44 @@ public class MessageTaskServiceTest
         var messageTaskService = new Mock<MessageTaskService>(caller.Object);
         await messageTaskService.Object.SendTemplateMessageByExternalAsync(options);
         caller.Verify(provider => provider.PostAsync(requestUri, options, true, default), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task TestGetListAsync()
+    {
+        var options = new GetMessageTaskModel(1, 10);
+        var data = new PaginatedListModel<MessageTaskModel>();
+        var requestUri = $"api/message-task";
+        var caller = new Mock<ICaller>();
+        caller.Setup(provider => provider.GetAsync<GetMessageTaskModel, PaginatedListModel<MessageTaskModel>>(requestUri, options, default)).ReturnsAsync(data).Verifiable();
+        var service = new Mock<MessageTaskService>(caller.Object);
+        var result = await service.Object.GetListAsync(options);
+        caller.Verify(provider => provider.GetAsync<GetMessageTaskModel, PaginatedListModel<MessageTaskModel>>(requestUri, options, default), Times.Once);
+        Assert.IsTrue(result is not null);
+    }
+
+    [TestMethod]
+    public async Task TestDeleteAsync()
+    {
+        Guid id = Guid.NewGuid();
+        var requestUri = $"api/message-task/{id}";
+        var caller = new Mock<ICaller>();
+        caller.Setup(provider => provider.DeleteAsync(requestUri, null, true, default)).Verifiable();
+        var service = new Mock<MessageTaskService>(caller.Object);
+        await service.Object.DeleteAsync(id);
+        caller.Verify(provider => provider.DeleteAsync(requestUri, null, true, default), Times.Once);
+    }
+
+    [TestMethod]
+    public async Task TestUpdateAsync()
+    {
+        Guid id = Guid.NewGuid();
+        var options = new MessageTaskUpsertModel();
+        var requestUri = $"api/message-task/{id}";
+        var caller = new Mock<ICaller>();
+        caller.Setup(provider => provider.PutAsync(requestUri, options, true, default)).Verifiable();
+        var service = new Mock<MessageTaskService>(caller.Object);
+        await service.Object.UpdateAsync(id, options);
+        caller.Verify(provider => provider.PutAsync(requestUri, options, true, default), Times.Once);
     }
 }
