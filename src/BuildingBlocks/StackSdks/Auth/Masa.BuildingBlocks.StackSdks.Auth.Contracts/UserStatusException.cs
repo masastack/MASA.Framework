ec5d603c4f69f4109e8bc500e46daf3ff369f4d4
Mayue <mayue@lonsid.cn>
2022-12-20 17:21:39 +0800

    feat:add Serializable attribute

--- src/BuildingBlocks/StackSdks/Auth/Masa.BuildingBlocks.StackSdks.Auth.Contracts/UserStatusException.cs
+++ src/BuildingBlocks/StackSdks/Auth/Masa.BuildingBlocks.StackSdks.Auth.Contracts/UserStatusException.cs
@@ -1,8 +1,11 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+using System.Runtime.Serialization;
+
 namespace Masa.BuildingBlocks.StackSdks.Auth.Contracts;
 
+[Serializable]
 public class UserStatusException : UserFriendlyException
 {
     public UserStatusException(string message) : base(message)
@@ -11,6 +14,11 @@ public class UserStatusException : UserFriendlyException
 
     public UserStatusException(string errorCode, params object[] parameters)
             : base(errorCode, null, parameters)
+    {
+    }
+
+    protected UserStatusException(SerializationInfo serializationInfo, StreamingContext context)
+        : base(serializationInfo, context)
     {
     }
 }
