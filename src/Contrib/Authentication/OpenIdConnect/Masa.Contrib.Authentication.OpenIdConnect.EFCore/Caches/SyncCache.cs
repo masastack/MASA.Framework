6f7c40084b5ffceb34f23e19225c097bdc20b3fb
wuweilai <824255785@qq.com>
2022-12-22 10:08:02 +0800

    refactor code

--- src/Contrib/Authentication/OpenIdConnect/Masa.Contrib.Authentication.OpenIdConnect.EFCore/Caches/SyncCache.cs
+++ src/Contrib/Authentication/OpenIdConnect/Masa.Contrib.Authentication.OpenIdConnect.EFCore/Caches/SyncCache.cs
@@ -1,6 +1,8 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+using Masa.BuildingBlocks.Authentication.OpenIdConnect.Domain.Entities;
+
 namespace Masa.Contrib.Authentication.OpenIdConnect.EFCore.Caches;
 
 public class SyncCache
@@ -71,56 +73,29 @@ public class SyncCache
 
     private async Task<IEnumerable<Client>> ClientQueryAsync()
     {
-        var datas = await(from client in _context.Set<Client>()
-
-                          join clientProperty in _context.Set<ClientProperty>()
-                          on client.Id equals clientProperty.ClientId into clientPropertyJoin
-                          from clientProperty in clientPropertyJoin.DefaultIfEmpty()
-
-                          join clientClaim in _context.Set<ClientClaim>()
-                          on client.Id equals clientClaim.ClientId into clientClaimJoin
-                          from clientClaim in clientClaimJoin.DefaultIfEmpty()
-
-                          join clientIdPRestriction in _context.Set<ClientIdPRestriction>()
-                          on client.Id equals clientIdPRestriction.ClientId into clientIdPRestrictionJoin
-                          from clientIdPRestriction in clientIdPRestrictionJoin.DefaultIfEmpty()
-
-                          join clientCorsOrigin in _context.Set<ClientCorsOrigin>()
-                          on client.Id equals clientCorsOrigin.ClientId into clientCorsOriginJoin
-                          from clientCorsOrigin in clientCorsOriginJoin.DefaultIfEmpty()
-
-                          join clientSecret in _context.Set<ClientSecret>()
-                          on client.Id equals clientSecret.ClientId into clientSecretJoin
-                          from clientSecret in clientSecretJoin.DefaultIfEmpty()
-
-                          select new
-                          {
-                              client,
-                              clientProperty,
-                              clientClaim,
-                              clientIdPRestriction,
-                              clientCorsOrigin,
-                              clientSecret,
-                          }).ToListAsync();
-
+        var clients = await _context.Set<Client>().ToListAsync();     
+        var clientPropertys = await _context.Set<ClientProperty>().ToListAsync();
+        var clientClaims = await _context.Set<ClientClaim>().ToListAsync();
+        var clientIdPRestrictions = await _context.Set<ClientIdPRestriction>().ToListAsync();
+        var clientCorsOrigins = await _context.Set<ClientCorsOrigin>().ToListAsync();
+        var clientSecrets = await _context.Set<ClientSecret>().ToListAsync();
         var clientGrantTypes = await _context.Set<ClientGrantType>().ToListAsync();
         var clientRedirectUris = await _context.Set<ClientRedirectUri>().ToListAsync();
         var clientPostLogoutRedirectUris = await _context.Set<ClientPostLogoutRedirectUri>().ToListAsync();
         var clientScopes = await _context.Set<ClientScope>().ToListAsync();
-        var clients = datas.GroupBy(data => data.client.Id).Select(gp =>
+
+        foreach(var client in clients)
         {
-            var client = gp.First().client;
             client.AllowedGrantTypes.AddRange(clientGrantTypes.Where(item => item.ClientId == client.Id));
             client.RedirectUris.AddRange(clientRedirectUris.Where(item => item.ClientId == client.Id));
             client.PostLogoutRedirectUris.AddRange(clientPostLogoutRedirectUris.Where(item => item.ClientId == client.Id));
-            client.Properties.AddRange(gp.Where(item => item.clientProperty is not null).Select(item => item.clientProperty));
-            client.Claims.AddRange(gp.Where(item => item.clientClaim is not null).Select(item => item.clientClaim));
-            client.IdentityProviderRestrictions.AddRange(gp.Where(item => item.clientIdPRestriction is not null).Select(item => item.clientIdPRestriction));
-            client.AllowedCorsOrigins.AddRange(gp.Where(item => item.clientCorsOrigin is not null).Select(item => item.clientCorsOrigin));
-            client.ClientSecrets.AddRange(gp.Where(item => item.clientSecret is not null).Select(item => item.clientSecret));
+            client.Properties.AddRange(clientPropertys.Where(item => item.ClientId == client.Id));
+            client.Claims.AddRange(clientClaims.Where(item => item.ClientId == client.Id));
+            client.IdentityProviderRestrictions.AddRange(clientIdPRestrictions.Where(item => item.ClientId == client.Id));
+            client.AllowedCorsOrigins.AddRange(clientCorsOrigins.Where(item => item.ClientId == client.Id));
+            client.ClientSecrets.AddRange(clientSecrets.Where(item => item.ClientId == client.Id));
             client.AllowedScopes.AddRange(clientScopes.Where(item => item.ClientId == client.Id));
-            return client;
-        });
+        }
 
         return clients;
     }
