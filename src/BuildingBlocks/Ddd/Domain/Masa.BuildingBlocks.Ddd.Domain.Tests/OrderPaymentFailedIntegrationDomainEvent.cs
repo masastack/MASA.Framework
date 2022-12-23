1b8c7a037e12ed4ff667e9ff283e8bdd7c014ff0
zhenlei520 <wangzhenlei520@gmail.com>
2022-12-23 14:44:17 +0800

    pref: Optimizing integration events must reload Topic

---
+++ src/BuildingBlocks/Ddd/Domain/Masa.BuildingBlocks.Ddd.Domain.Tests/OrderPaymentFailedIntegrationDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.Ddd.Domain.Tests;
+
+public record OrderPaymentFailedIntegrationDomainEvent: IntegrationDomainEvent
+{
+    public override string Topic { get; set; } = "PaymentFailed";
+
+    public Guid OrderId { get; set; }
+}
--- src/BuildingBlocks/Ddd/Domain/Masa.BuildingBlocks.Ddd.Domain.Tests/_Imports.cs
+++ src/BuildingBlocks/Ddd/Domain/Masa.BuildingBlocks.Ddd.Domain.Tests/_Imports.cs
@@ -2,5 +2,6 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.Ddd.Domain.Entities;
+global using Masa.BuildingBlocks.Ddd.Domain.Events;
 global using Masa.BuildingBlocks.Ddd.Domain.Values;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
