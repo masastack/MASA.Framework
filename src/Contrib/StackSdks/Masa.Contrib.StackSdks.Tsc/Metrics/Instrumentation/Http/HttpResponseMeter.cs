dbf6bd83d6fa124803c47ffeadef5d63fc320cf3
qinyouzeng <qinyouzeng@masastack.com>
2023-01-04 09:30:03 +0800

    feat: update metric

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Tsc/Metrics/Instrumentation/Http/HttpResponseMeter.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Tsc/Metrics/Instrumentation/Http/HttpResponseMeter.cs
@@ -5,37 +5,11 @@ namespace Masa.Contrib.StackSdks.Tsc.Metrics.Instrumentation.Http;
 
 public class HttpResponseMeter : IHttpResponseMetric
 {
-    private readonly Dictionary<string, Counter<long>> _dicCounts = new();
-    private readonly Meter _meter;
+    private readonly Histogram<double> responseHistogram;
 
     public HttpResponseMeter(Meter meter)
     {
-        _meter = meter;
-    }
-
-    private Counter<long> CreateRequestCounter() => _meter.CreateCounter<long>("masa.http.request.count", "", "http request in count per seconds");
-
-    private Counter<long> CreateReponseCounter() => _meter.CreateCounter<long>("masa.http.response.count", "", "http response in count per seconds");
-
-    private IEnumerable<KeyValuePair<string, object?>> GetTags(HttpRequest httpRequest)
-    {
-        var list = new List<KeyValuePair<string, object?>>
-        {
-            new KeyValuePair<string, object?>("http.target", httpRequest.Path),
-            new KeyValuePair<string, object?>("http.method", httpRequest.Method)
-        };
-        return list;
-    }
-
-    private IEnumerable<KeyValuePair<string, object?>> GetTags(HttpResponse httpResponse)
-    {
-        var list = new List<KeyValuePair<string, object?>>
-        {
-            new KeyValuePair<string, object?>("http.status_code", httpResponse.StatusCode),
-            new KeyValuePair<string, object?>("http.method", httpResponse.HttpContext.Request.Method),
-            new KeyValuePair<string, object?>("http.target", httpResponse.HttpContext.Request.Path)
-        };
-        return list;
+        responseHistogram = meter.CreateHistogram<double>("http.response", "", "http response duration histogram");
     }
 
     public void Handle(HttpResponse httpResponse)
@@ -45,37 +19,13 @@ public class HttpResponseMeter : IHttpResponseMetric
         if (string.IsNullOrEmpty(path))
             return;
 
-        Counter<long> counter;
-        if (_dicCounts.ContainsKey(path))
+        var tags = new KeyValuePair<string, object?>[]
         {
-            counter = _dicCounts[path];
-        }
-        else
-        {
-            counter = CreateReponseCounter();
-            _dicCounts.Add(path, counter);
-        }
-        var tags = GetTags(httpResponse);
-        counter.Add(1, tags.ToArray());
-    }
-
-    public void Handle(HttpRequest httpRequest)
-    {
-        var path = httpRequest.Path;
-        if (string.IsNullOrEmpty(path))
-            return;
-
-        Counter<long> counter;
-        if (_dicCounts.ContainsKey(path))
-        {
-            counter = _dicCounts[path];
-        }
-        else
-        {
-            counter = CreateRequestCounter();
-            _dicCounts.Add(path, counter);
-        }
-        var tags = GetTags(httpRequest);
-        counter.Add(1, tags.ToArray());
+            new KeyValuePair<string, object?>("http.status_code", httpResponse.StatusCode),
+            new KeyValuePair<string, object?>("http.method", httpResponse.HttpContext.Request.Method),
+            new KeyValuePair<string, object?>("http.scheme", httpResponse.HttpContext.Request.Scheme),
+            new KeyValuePair<string, object?>("http.target", httpResponse.HttpContext.Request.Path)
+        };
+        responseHistogram.Record(Activity.Current!.Duration.TotalMilliseconds, tags.ToArray());
     }
 }
