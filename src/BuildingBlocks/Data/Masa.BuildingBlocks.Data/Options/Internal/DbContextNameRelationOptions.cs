fd971fee6d7e8becf8b4660fdd82285c78564db2
zhenlei520 <wangzhenlei520@gmail.com>
2023-01-13 22:43:08 +0800

    pref(ConnectionStringName): Optimize multiple acquisitions of ConnectionStringName based on DbContextType

---
+++ src/BuildingBlocks/Data/Masa.BuildingBlocks.Data/Options/Internal/DbContextNameRelationOptions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+// ReSharper disable once CheckNamespace
+
+namespace Masa.BuildingBlocks.Data;
+
+internal class DbContextNameRelationOptions
+{
+    public string Name { get; }
+
+    public Type DbContextType { get; }
+
+    public DbContextNameRelationOptions(string name, Type dbContextType)
+    {
+        Name = name;
+        DbContextType = dbContextType;
+    }
+}
---
+++ src/BuildingBlocks/Data/Tests/Masa.BuildingBlocks.Data.Tests/ConnectionStringNameTest.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.Data.Tests;
+
+[TestClass]
+public class ConnectionStringNameTest
+{
+    [TestMethod]
+    public void TestGetConnectionStringName()
+    {
+        string name = ConnectionStringNameAttribute.GetConnStringName<CustomDbContext>();
+        Assert.AreEqual(ConnectionStrings.DEFAULT_CONNECTION_STRING_NAME, name);
+
+        name = ConnectionStringNameAttribute.GetConnStringName<CustomQueryDbContext>();
+        Assert.AreEqual(typeof(CustomQueryDbContext).FullName, name);
+
+        name = ConnectionStringNameAttribute.GetConnStringName<CustomQuery2DbContext>();
+        Assert.AreEqual(typeof(CustomQuery2DbContext).FullName, name);
+
+        name = ConnectionStringNameAttribute.GetConnStringName<CustomQuery3DbContext>();
+        Assert.AreEqual("query3", name);
+    }
+}
