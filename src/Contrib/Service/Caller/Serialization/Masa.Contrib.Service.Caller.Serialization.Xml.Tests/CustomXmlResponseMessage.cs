67a02ef35b0a31e66b0775d9a279d30b13d5ac4d
zhenlei520 <wangzhenlei520@gmail.com>
2023-01-13 15:07:03 +0800

    test(Caller): Complementary unit tests

---
+++ src/Contrib/Service/Caller/Serialization/Masa.Contrib.Service.Caller.Serialization.Xml.Tests/CustomXmlResponseMessage.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Service.Caller.Serialization.Xml.Tests;
+
+public class CustomXmlResponseMessage : XmlResponseMessage
+{
+    public Task<TResponse?> GetCustomResponseAsync<TResponse>(
+        HttpContent httpContent,
+        CancellationToken cancellationToken = default)
+        => base.FormatResponseAsync<TResponse>(httpContent, cancellationToken);
+}
