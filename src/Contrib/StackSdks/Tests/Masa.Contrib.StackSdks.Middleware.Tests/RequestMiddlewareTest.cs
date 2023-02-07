04725c47c2e33ac2253834268723a1742a51a90b
Mayue <mayue@lonsid.cn>
2023-02-07 14:35:55 +0800

    feat:add middleware test

---
+++ src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Middleware.Tests/RequestMiddlewareTest.cs
@@ -0,0 +1,13 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.StackSdks.Middleware.Tests;
+
+[TestClass]
+public class RequestMiddlewareTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
