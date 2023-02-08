a56134ca382321041848018f0a8f4f38f5d3ea3f
磊磊 <wangzhenlei520@gmail.com>
2023-02-08 20:14:12 +0800

    pref(DDD): Optimize the experience of using DomainService (#438)
    * feature(DDD): Adjust IGenerateDomainEvents responsibility
    
    * pref(DDD): Optimize the experience of using DomainService

--- src/BuildingBlocks/Ddd/Domain/Masa.BuildingBlocks.Ddd.Domain/Services/DomainService.cs
+++ src/BuildingBlocks/Ddd/Domain/Masa.BuildingBlocks.Ddd.Domain/Services/DomainService.cs
@@ -7,7 +7,17 @@ namespace Masa.BuildingBlocks.Ddd.Domain.Services;
 
 public class DomainService : IDomainService
 {
-    public IDomainEventBus EventBus { get; }
+    public IDomainEventBus EventBus { get; private set; }
+
+    public DomainService()
+    {
+        EventBus = null!;
+    }
 
     public DomainService(IDomainEventBus eventBus) => EventBus = eventBus;
+
+    public void SetDomainEventBus(IDomainEventBus domainEventBus)
+    {
+        EventBus = domainEventBus;
+    }
 }
