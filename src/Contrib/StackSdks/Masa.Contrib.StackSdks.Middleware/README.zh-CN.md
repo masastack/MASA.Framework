88effdb717ff309adb9c436d4af7f4defb21a5bd
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-02-08 20:26:52 +0800

    Feat/stack middleware (#440)
    * feat:add stack middleware
    
    * feat:add middleware test
    
    * feat:add unit test
    
    * fix:code smells
    
    * fix:smell
    
    * feat:add readme
    
    * doc：typo
    
    * feat:remove ValidatorEventMiddleware
    
    * doc:style
    
    ---------
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

---
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Middleware/README.zh-CN.md
@@ -0,0 +1,35 @@
+中 | [EN](README.md)
+
+## Masa.Contrib.StackSdks.Middleware
+
+用例：
+
+```C#
+Install-Package Masa.Contrib.StackSdks.Middleware
+```
+
+```C#
+builder.Services.AddStackMiddleware();
+
+...
+
+app.UseAddStackMiddleware();
+```
+
+`Masa.Contrib.StackSdks.Middleware`提供了`IDisabledEventDeterminer`和`IDisabledRequestDeterminer`的默认实现，依赖`Masa.Contrib.Authentication.Identity`,`Masa.Contrib.StackSdks.Config`。`AddStackMiddleware`前应完成相应代码的初始化。
+
+可以根据业务需要重写`IDisabledEventDeterminer`和`IDisabledRequestDeterminer`替换默认实现。
+
+```C#
+builder.Services.AddScoped<IDisabledEventDeterminer, CustomizeDisabledEventDeterminer>();
+
+builder.Services.AddStackMiddleware();
+```
+
+### DisabledEventMiddleware
+
+根据`IDisabledRequestDeterminer`的`Determiner`方法返回值，判断特性`DisabledRouteAttribute`，路由元数据包含该特性的请求禁用。
+
+### DisabledRequestMiddleware
+
+根据`IDisabledEventDeterminer`的`Determiner`方法返回值和`DisabledCommand`属性(默认实现为true,禁用所有Command),真的特殊需要放行实现添加`AllowedEventAttribute`特性。
---
+++ src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Middleware.Tests/EventMiddlewareTest.cs
@@ -0,0 +1,64 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.StackSdks.Middleware.Tests;
+
+[TestClass]
+public class EventMiddlewareTest
+{
+    private IServiceProvider _serviceProvider;
+    private readonly IEventBus _eventBus;
+
+    public EventMiddlewareTest()
+    {
+        var builder = WebApplication.CreateBuilder();
+
+        builder.Services.AddMasaIdentity(options =>
+        {
+            options.Mapping(nameof(MasaUser.Account), "ACCOUNT");
+        });
+
+        builder.Services.AddMasaStackConfig();
+        builder.Services.AddTestEventBus(new Assembly[1] { Assembly.GetExecutingAssembly() }, ServiceLifetime.Scoped);
+        builder.Services.AddStackMiddleware();
+
+        _serviceProvider = builder.Services.BuildServiceProvider();
+        var httpContextAccessor = _serviceProvider.GetRequiredService<IHttpContextAccessor>();
+        httpContextAccessor.HttpContext = new DefaultHttpContext()
+        {
+            User = new ClaimsPrincipal(new List<ClaimsIdentity>()
+            {
+                new(new List<Claim>()
+                {
+                    new(ClaimType.DEFAULT_USER_ID, "1"),
+                    new("ACCOUNT", "guest")
+                })
+            })
+        };
+
+        _eventBus = _serviceProvider.GetRequiredService<IEventBus>();
+    }
+
+    [TestMethod]
+    public void TestCommand()
+    {
+        var command = new TestCommand();
+        Assert.ThrowsExceptionAsync<UserFriendlyException>(async () => await _eventBus.PublishAsync(command), "DISABLED_OPERATE");
+    }
+
+    [TestMethod]
+    public async Task TestAllowCommand()
+    {
+        var command = new TestAllowCommand();
+        await _eventBus.PublishAsync(command);
+        Assert.AreEqual(1, command.Count);
+    }
+
+    [TestMethod]
+    public async Task TestQuery()
+    {
+        var query = new TestQuery();
+        await _eventBus.PublishAsync(query);
+        Assert.IsTrue(query.Result);
+    }
+}
