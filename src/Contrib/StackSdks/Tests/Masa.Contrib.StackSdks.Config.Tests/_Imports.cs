228f6da6e96330b6cdcea3c3e4e2d3695f03c9de
yanpengju <yanpengju@masastack.com>
2023-02-15 15:57:23 +0800

    feat: update test

--- src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Config.Tests/_Imports.cs
+++ src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Config.Tests/_Imports.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 global using Masa.BuildingBlocks.StackSdks.Config;
-global using Masa.Contrib.Configuration.ConfigurationApi.Dcc.Options;
 global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
+
