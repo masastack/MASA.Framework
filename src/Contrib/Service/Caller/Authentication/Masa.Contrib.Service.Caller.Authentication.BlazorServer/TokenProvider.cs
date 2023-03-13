ec7b89a2e23e0b30e0f829cc54ac44d7b95f08b1
zhenlei520 <wangzhenlei520@gmail.com>
2023-03-13 16:49:36 +0800

    chore: modify namespace

--- src/Contrib/Service/Caller/Authentication/Masa.Contrib.Service.Caller.Authentication.BlazorServer/TokenProvider.cs
+++ src/Contrib/Service/Caller/Authentication/Masa.Contrib.Service.Caller.Authentication.BlazorServer/TokenProvider.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Contrib.Service.Caller.Authentication;
+namespace Masa.Contrib.Service.Caller.Authentication.BlazorServer;
 
 [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public class TokenProvider
