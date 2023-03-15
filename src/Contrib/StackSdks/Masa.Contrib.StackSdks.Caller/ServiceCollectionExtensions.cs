55b491f737687718af32fc240f0afdf11f265dca
Mayue <mayue8373@live.com>
2023-03-15 18:12:27 +0800

    feat：Func

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Caller/ServiceCollectionExtensions.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Caller/ServiceCollectionExtensions.cs
@@ -8,15 +8,15 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddStackCaller(
         this IServiceCollection services,
         Assembly assembly,
-        Func<TokenProvider> tokenProvider,
+        Func<IServiceProvider, TokenProvider> tokenProvider,
         Action<JwtTokenValidatorOptions> jwtTokenValidatorOptions,
-        Action<ClientRefreshTokenOptions>? clientRefreshTokenOptions)
+        Action<ClientRefreshTokenOptions>? clientRefreshTokenOptions = null)
     {
         MasaArgumentException.ThrowIfNull(jwtTokenValidatorOptions);
 
         services.Configure(jwtTokenValidatorOptions);
         services.Configure(clientRefreshTokenOptions);
-        services.AddScoped((serviceProvider) => { return tokenProvider.Invoke(); });
+        services.AddScoped((serviceProvider) => { return tokenProvider.Invoke(serviceProvider); });
         services.AddSingleton<JwtTokenValidator>();
         services.AddAutoRegistrationCaller(assembly);
         return services;
