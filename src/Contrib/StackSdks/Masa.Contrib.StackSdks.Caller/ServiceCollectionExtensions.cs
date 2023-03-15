6a7494d54f5c9515c09eb7b42ff5de46a49ad07a
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-03-15 18:27:48 +0800

    feat:add stack caller (#502)
    * feat:add stack caller
    
    * feat:delete stackcaller buildingblock
    
    * feat:delete stackcaller test
    
    * feat:pr
    
    * refactor:tokenprovider
    
    * feat：Func

---
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Caller/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.StackSdks.Caller;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddStackCaller(
+        this IServiceCollection services,
+        Assembly assembly,
+        Func<IServiceProvider, TokenProvider> tokenProvider,
+        Action<JwtTokenValidatorOptions> jwtTokenValidatorOptions,
+        Action<ClientRefreshTokenOptions>? clientRefreshTokenOptions = null)
+    {
+        MasaArgumentException.ThrowIfNull(jwtTokenValidatorOptions);
+
+        services.Configure(jwtTokenValidatorOptions);
+        services.Configure(clientRefreshTokenOptions);
+        services.AddScoped((serviceProvider) => { return tokenProvider.Invoke(serviceProvider); });
+        services.AddSingleton<JwtTokenValidator>();
+        services.AddAutoRegistrationCaller(assembly);
+        return services;
+    }
+}
---
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Caller/StackHttpClientCaller.cs
@@ -0,0 +1,16 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.StackSdks.Caller;
+
+public abstract class StackHttpClientCaller : HttpClientCallerBase
+{
+    protected override void UseHttpClientPost(MasaHttpClientBuilder masaHttpClientBuilder)
+    {
+        masaHttpClientBuilder.UseAuthentication(serviceProvider => new AuthenticationService(
+                serviceProvider.GetRequiredService<TokenProvider>(),
+                serviceProvider.GetRequiredService<JwtTokenValidator>()
+            ));
+        base.UseHttpClientPost(masaHttpClientBuilder);
+    }
+}
