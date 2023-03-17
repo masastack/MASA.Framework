7d9f25ac27847a100618d544540264585b279eb7
wzh425 <wuzihao425@outlook.com>
2023-03-17 16:12:17 +0800

    style :  global using

--- src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Alert.Tests/AlertClientTest.cs
+++ src/Contrib/StackSdks/Tests/Masa.Contrib.StackSdks.Alert.Tests/AlertClientTest.cs
@@ -1,9 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-using Masa.BuildingBlocks.StackSdks.Alert;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Masa.Contrib.StackSdks.Alert.Tests;
 
 [TestClass]
