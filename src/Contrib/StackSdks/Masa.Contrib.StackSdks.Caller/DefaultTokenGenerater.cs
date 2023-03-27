1c0f79a7e1ff7756200f656ad46b429a9641061e
wzh425 <wuzihao425@outlook.com>
2023-03-27 15:57:24 +0800

    refactor(stack-sdk) :  DefaultTokenGenerater processing token generator

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Caller/DefaultTokenGenerater.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Caller/DefaultTokenGenerater.cs
@@ -5,8 +5,30 @@ namespace Masa.Contrib.StackSdks.Caller;
 
 internal class DefaultTokenGenerater : ITokenGenerater
 {
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private const string SCHEME = "Bearer ";
+
+    public DefaultTokenGenerater(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
     public TokenProvider Generater()
     {
+        StringValues authenticationHeaderValue;
+
+        if (_httpContextAccessor.HttpContext?.Request.Headers.TryGetValue("Authorization", out authenticationHeaderValue) == true)
+        {
+            var accessToken = authenticationHeaderValue.ToString();
+
+            if (!string.IsNullOrEmpty(accessToken) && accessToken.ToString().IndexOf(SCHEME) > -1)
+            {
+                accessToken = accessToken.Replace(SCHEME, "");
+            }
+
+            return new TokenProvider { AccessToken = accessToken };
+        }
+
         return new TokenProvider();
     }
 }
