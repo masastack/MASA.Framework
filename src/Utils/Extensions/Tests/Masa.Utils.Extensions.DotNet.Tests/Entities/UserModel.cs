c7aeb580ed0f8f7f3e004e32b35df9663bd5bf5c
zhenlei520 <wangzhenlei520@gmail.com>
2023-04-18 01:33:38 +0800

    feat: System.Text.Json: dynamic converter supports .NET7.0, .NET8.0

---
+++ src/Utils/Extensions/Tests/Masa.Utils.Extensions.DotNet.Tests/Entities/UserModel.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Utils.Extensions.DotNet.Tests.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public int? Age2 { get; set; }
+
+    public bool Gender { get; set; }
+
+    public bool? Gender2 { get; set; }
+
+    public List<string> Tags { get; set; }
+
+    public int[] UserRoles { get; set; }
+
+    public UserClaimType UserClaimType { get; set; }
+
+    public object Avatar { get; set; }
+
+    public object Avatar2 { get; set; }
+
+    public DateTime CreateTime { get; set; }
+}
