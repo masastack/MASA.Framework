00c104d67023ed328744ab8cedd4fe6b113bd3ae
磊磊 <wangzhenlei520@gmail.com>
2023-04-17 13:27:40 +0800

    fix: Fix After disabling automatic mapping, the mapping route is incomplete (#562)
    * fix: Fix After disabling automatic mapping, the mapping route is incomplete
    
    * chore: Fix code smell

---
+++ src/Contrib/Service/Tests/Masa.Contrib.Service.MinimalAPIs.Tests/Services/PersonService.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Service.MinimalAPIs.Tests.Services;
+
+public class PersonService : ServiceBase
+{
+    public PersonService()
+    {
+        RouteOptions.DisableAutoMapRoute = true;
+        App.MapGet("list", GetListAsync);
+    }
+
+    private Task GetListAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
