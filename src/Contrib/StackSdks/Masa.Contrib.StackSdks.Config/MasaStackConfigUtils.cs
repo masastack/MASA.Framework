1ff08ed3b5f15bacdb941d30e9e6492b3ea9d70b
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-04-23 15:14:28 +0800

    Refactor/stackconfig (#577)
    * refactor(stack-config): update  stack config
    
    * feat:stack config
    
    * feat:modify appsettings and unit test
    
    * refactor:remove  useless code
    
    * feat:refatcor MasaStackConfigUtils
    
    * fix:code smell
    
    * feat:add MASA_STACK
    
    ---------
    
    Co-authored-by: yanpengju <yanpengju@masastack.com>

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Config/MasaStackConfigUtils.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Config/MasaStackConfigUtils.cs
@@ -1,65 +1,37 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Contrib.StackSdks.Config
+[assembly: InternalsVisibleTo("Masa.Contrib.StackSdks.Config.Tests")]
+namespace Masa.Contrib.StackSdks.Config;
+
+internal static class MasaStackConfigUtils
 {
-    public static class MasaStackConfigUtils
+    public static DccOptions GetDefaultDccOptions(Dictionary<string, string> configMap)
     {
-        public static Dictionary<string, JsonObject> GetAllServer(Dictionary<string, string> configMap)
+        var value = configMap.GetValueOrDefault(MasaStackConfigConstant.MASA_STACK) ?? "";
+        var data = JsonSerializer.Deserialize<JsonArray>(value) ?? new();
+        var dccServerAddress = data.FirstOrDefault(i => i?["id"]?.ToString() == MasaStackConstant.DCC)?[MasaStackConstant.SERVICE]?["host"]?.ToString() ?? "";
+        var redisStr = configMap.GetValueOrDefault(MasaStackConfigConstant.REDIS) ?? throw new Exception("redis options can not null");
+        var redis = JsonSerializer.Deserialize<RedisModel>(redisStr) ?? throw new JsonException();
+        var secret = configMap.GetValueOrDefault(MasaStackConfigConstant.DCC_SECRET);
+
+        var options = new DccOptions
         {
-            var value = configMap.GetValueOrDefault(MasaStackConfigConstant.MASA_SERVER);
-            if (string.IsNullOrEmpty(value))
+            Environment = configMap.GetValueOrDefault(MasaStackConfigConstant.ENVIRONMENT)!,
+            ManageServiceAddress = dccServerAddress,
+            RedisOptions = new Caching.Distributed.StackExchangeRedis.RedisConfigurationOptions
             {
-                return new();
-            }
-            return JsonSerializer.Deserialize<Dictionary<string, JsonObject>>(value) ?? new();
-        }
-
-        public static string GetServerDomain(Dictionary<string, string> configMap, string protocol, string project, string service)
-        {
-            var domain = "";
-            GetAllServer(configMap).TryGetValue(project, out JsonObject? jsonObject);
-            if (jsonObject != null)
+                Servers = new List<Caching.Distributed.StackExchangeRedis.RedisServerOptions>
             {
-                var secondaryDomain = jsonObject[service]?.ToString();
-                if (secondaryDomain != null)
-                {
-                    domain = $"{protocol}://{secondaryDomain}.{configMap.GetValueOrDefault(MasaStackConfigConstant.NAMESPACE)}";
-                }
-            }
-            return domain;
-        }
+                new Caching.Distributed.StackExchangeRedis.RedisServerOptions(redis.RedisHost,redis.RedisPort)
+            },
+                DefaultDatabase = redis.RedisDb,
+                Password = redis.RedisPassword
+            },
+            PublicSecret = secret,
+            ConfigObjectSecret = secret
+        };
 
-        public static string GetDccServiceDomain(Dictionary<string, string> configMap)
-        {
-            return GetServerDomain(configMap, HttpProtocol.HTTP, MasaStackConstant.DCC, MasaStackConstant.SERVER);
-        }
-
-        public static DccOptions GetDefaultDccOptions(Dictionary<string, string> configMap)
-        {
-            var dccServerAddress = GetDccServiceDomain(configMap);
-            var redisStr = configMap.GetValueOrDefault(MasaStackConfigConstant.REDIS) ?? throw new Exception("redis options can not null");
-            var redis = JsonSerializer.Deserialize<RedisModel>(redisStr) ?? throw new JsonException();
-            var secret = configMap.GetValueOrDefault(MasaStackConfigConstant.DCC_SECRET);
-
-            var options = new DccOptions
-            {
-                Environment = configMap.GetValueOrDefault(MasaStackConfigConstant.ENVIRONMENT)!,
-                ManageServiceAddress = dccServerAddress,
-                RedisOptions = new Caching.Distributed.StackExchangeRedis.RedisConfigurationOptions
-                {
-                    Servers = new List<Caching.Distributed.StackExchangeRedis.RedisServerOptions>
-                {
-                    new Caching.Distributed.StackExchangeRedis.RedisServerOptions(redis.RedisHost,redis.RedisPort)
-                },
-                    DefaultDatabase = redis.RedisDb,
-                    Password = redis.RedisPassword
-                },
-                PublicSecret = secret,
-                ConfigObjectSecret = secret
-            };
-
-            return options;
-        }
+        return options;
     }
 }
