baf8e0a27927c470ded2d15c05391ca19f06c9ce
磊磊 <wangzhenlei520@gmail.com>
2023-05-10 04:02:21 -0700

    fix: fix issues-594 (#611)
    * fix: fix issues-594
    
    * chore: Fix code smell
    
    * chore: Fix code smell

---
+++ src/Contrib/Ddd/Domain/Tests/Masa.Contrib.Ddd.Domain.Tests/Infrastructure/CustomLogger.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+// ReSharper disable once CheckNamespace
+
+namespace Masa.Contrib.Ddd.Domain.Tests;
+
+public class CustomLogger<T> : ILogger<T>
+{
+    public void Log<TState>(
+        LogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        DomainEventBusTest.ExecuteTimer++;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return true;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null!;
+    }
+}
