55e10fd84dce9d811c3f5184c6ae995686197844
wzh425 <wuzihao425@outlook.com>
2023-07-04 14:50:06 +0800

    feat : SchedulerTaskService add NotifyRunResultAsync

---
+++ src/BuildingBlocks/StackSdks/Masa.BuildingBlocks.StackSdks.Scheduler/Enum/TaskRunResultStatus.cs
@@ -0,0 +1,12 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.StackSdks.Scheduler.Enum;
+
+public enum TaskRunResultStatus
+{
+    Success = 3,
+    Failure = 4,
+    TimeoutSuccess = 6,
+    Ignore = 9
+}
