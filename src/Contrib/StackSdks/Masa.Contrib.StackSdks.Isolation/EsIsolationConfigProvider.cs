4a42974583577c07b010844d2ff3af748339bd6d
Mayue <mayue8373@live.com>
2023-07-13 10:59:50 +0800

    fix:es provider missing key

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Isolation/EsIsolationConfigProvider.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Isolation/EsIsolationConfigProvider.cs
@@ -8,6 +8,7 @@ public class EsIsolationConfigProvider
     readonly IMultiEnvironmentContext _multiEnvironmentContext;
     readonly EnvironmentProvider _environmentProvider;
     readonly IMultiEnvironmentMasaStackConfig _multiEnvironmentMasaStackConfig;
+    readonly IMasaStackConfig _masaStackConfig;
     readonly ILogger<EsIsolationConfigProvider>? _logger;
 
     readonly Dictionary<string, ElasticModel> _esOptions = new();
@@ -16,12 +17,14 @@ public class EsIsolationConfigProvider
         EnvironmentProvider environmentProvider,
         ILogger<EsIsolationConfigProvider>? logger,
         IMultiEnvironmentContext multiEnvironmentContext,
-        IMultiEnvironmentMasaStackConfig multiEnvironmentMasaStackConfig)
+        IMultiEnvironmentMasaStackConfig multiEnvironmentMasaStackConfig,
+        IMasaStackConfig masaStackConfig)
     {
         _multiEnvironmentContext = multiEnvironmentContext;
         _environmentProvider = environmentProvider;
         _multiEnvironmentMasaStackConfig = multiEnvironmentMasaStackConfig;
         _logger = logger;
+        _masaStackConfig = masaStackConfig;
 
         InitData();
     }
@@ -40,6 +43,21 @@ public class EsIsolationConfigProvider
 
     public ElasticModel GetEsOptions()
     {
-        return _esOptions[_multiEnvironmentContext.CurrentEnvironment];
+        if (!_esOptions.Any())
+        {
+            throw new UserFriendlyException("Missing es configuration, please check the configuration of MasaStackConfig");
+        }
+        if (_esOptions.ContainsKey(_multiEnvironmentContext.CurrentEnvironment))
+        {
+            return _esOptions[_multiEnvironmentContext.CurrentEnvironment];
+        }
+        if (_esOptions.ContainsKey(_masaStackConfig.Environment))
+        {
+            _logger?.LogError("The current environment does not match the es configuration, use the configuration of the _masaStackConfig.Environment {Envionment}", _masaStackConfig.Environment);
+            return _esOptions[_masaStackConfig.Environment];
+        }
+
+        _logger?.LogError("No es configuration is matched, the first item is used by default");
+        return _esOptions.First().Value;
     }
 }
