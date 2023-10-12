a97ca482116ec04722849070f3d56505a71c86fb
wzh425 <wuzihao425@outlook.com>
2023-10-12 09:53:19 +0800

    reactor :  GetUnreadAsync  add tag

---
+++ src/BuildingBlocks/StackSdks/Masa.BuildingBlocks.StackSdks.Mc/Model/GetUnreadModel.cs
@@ -0,0 +1,11 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.BuildingBlocks.StackSdks.Mc.Model;
+
+public class GetUnreadModel
+{
+    public string ChannelCode { get; set; } = string.Empty;
+
+    public string Tag { get; set; } = string.Empty;
+}
