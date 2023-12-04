71ca21b6e1ade269f1c31dcd5975631b4b9b35b4
qinyouzeng <qinyouzeng@masastack.com>
2023-12-04 09:41:25 +0800

    fix: fix extension method

--- src/Contrib/StackSdks/Masa.Contrib.StackSdks.Tsc.OpenTelemetry/Traceing/ServiceExtensions.cs
+++ src/Contrib/StackSdks/Masa.Contrib.StackSdks.Tsc.OpenTelemetry/Traceing/ServiceExtensions.cs
@@ -1,8 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using StackExchange.Redis;
-
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static partial class ServiceExtensions
