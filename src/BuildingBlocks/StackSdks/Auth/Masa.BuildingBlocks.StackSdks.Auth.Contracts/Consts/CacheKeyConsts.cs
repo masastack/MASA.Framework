ae0d54ad1aabf4a057b324aa6bd072cdee225f68
duia <532977252@qq.com>
2024-04-25 13:47:30 +0800

    fix:remove sdk

--- src/BuildingBlocks/StackSdks/Auth/Masa.BuildingBlocks.StackSdks.Auth.Contracts/Consts/CacheKeyConsts.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-namespace Masa.BuildingBlocks.StackSdks.Auth.Contracts.Consts;
-
-public static class CacheKeyConsts
-{
-    public const string ALL_THIRD_PARTY_IDP = "get_all_thirdparty_idp";
-}
--- src/BuildingBlocks/StackSdks/Masa.BuildingBlocks.StackSdks.Mc/Model/EntityModel.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-namespace Masa.BuildingBlocks.StackSdks.Mc.Model;
-
-public class EntityModel<TKey>
-{
-    public TKey Id { get; set; } = default!;
-}
