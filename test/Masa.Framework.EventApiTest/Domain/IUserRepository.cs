ad2e5d29397edd9e8f510bc43c48aa738d9e6901
duia <532977252@qq.com>
2024-05-08 11:25:46 +0800

    feat:remove test api

--- test/Masa.Framework.EventApiTest/Domain/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-namespace Masa.Framework.EventApiTest.Domain
-{
-    public interface IUserRepository : IRepository<User>
-    {
-    }
-}
