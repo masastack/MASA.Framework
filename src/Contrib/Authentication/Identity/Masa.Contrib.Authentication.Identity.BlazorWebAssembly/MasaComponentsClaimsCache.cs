0a6dc69a8b1f58a86b250f4b3fe3f6711f8289b0
wzh425 <wuzihao425@outlook.com>
2024-08-28 10:24:54 +0800

    🐛 fix: Fix BlazorCurrentPrincipalAccessor in BlazorWebAssembly

---
+++ src/Contrib/Authentication/Identity/Masa.Contrib.Authentication.Identity.BlazorWebAssembly/MasaComponentsClaimsCache.cs
@@ -0,0 +1,32 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Authentication.Identity.BlazorWebAssembly;
+
+public class MasaComponentsClaimsCache : IScopedDependency
+{
+    public ClaimsPrincipal Principal { get; private set; }
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+
+    public MasaComponentsClaimsCache(
+        IClientScopeServiceProviderAccessor serviceProviderAccessor)
+    {
+        _authenticationStateProvider = serviceProviderAccessor.ServiceProvider.GetService<AuthenticationStateProvider>();
+        if (_authenticationStateProvider != null)
+        {
+            _authenticationStateProvider.AuthenticationStateChanged += async (task) =>
+            {
+                Principal = (await task).User;
+            };
+        }
+    }
+
+    public virtual async Task InitializeAsync()
+    {
+        if (_authenticationStateProvider != null)
+        {
+            var authenticationState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+            Principal = authenticationState.User;
+        }
+    }
+}
