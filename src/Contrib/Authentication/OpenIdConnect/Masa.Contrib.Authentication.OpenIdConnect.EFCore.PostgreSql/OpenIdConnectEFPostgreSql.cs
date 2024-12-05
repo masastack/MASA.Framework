2d9fa3b241ebc1e0c4cd8eaf8ba70af353555c0d
wzh425 <wuzihao425@outlook.com>
2024-12-05 15:58:57 +0800

    feat: OpenIdConnect supports PostgreSQL

---
+++ src/Contrib/Authentication/OpenIdConnect/Masa.Contrib.Authentication.OpenIdConnect.EFCore.PostgreSql/OpenIdConnectEFPostgreSql.cs
@@ -0,0 +1,9 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Contrib.Authentication.OpenIdConnect.EFCore.PostgreSql;
+
+public static class OpenIdConnectEFPostgreSql
+{
+    public static Assembly Assembly => typeof(OpenIdConnectEFPostgreSql).Assembly;
+}
---
+++ src/Contrib/Authentication/OpenIdConnect/Masa.Contrib.Authentication.OpenIdConnect.EFCore.PostgreSql/_Imports.cs
@@ -0,0 +1,7 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+global using Masa.BuildingBlocks.Authentication.OpenIdConnect.Domain.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using System.Reflection;
