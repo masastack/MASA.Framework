9a6176188b9704704cf0676b080238681fe38df8
MayueCif <mayue8373@live.com>
2026-03-04 14:42:50 +0800

    移除DeviceFlowCodes和PersistedGrant的命名空间
    移除了 DeviceFlowCodes.cs 和 PersistedGrant.cs 文件中的命名空间声明，使这两个类不再属于原有的命名空间。此更改可能用于调整类的作用域或为后续的命名空间重构做准备。

--- src/BuildingBlocks/Authentication/OpenIdConnect/Masa.BuildingBlocks.Authentication.OpenIdConnect.Domain/Entities/DeviceFlowCodes.cs
+++ src/BuildingBlocks/Authentication/OpenIdConnect/Masa.BuildingBlocks.Authentication.OpenIdConnect.Domain/Entities/DeviceFlowCodes.cs
@@ -1,8 +1,6 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.BuildingBlocks.Authentication.OpenIdConnect.Domain.Entities;
-
 public class DeviceFlowCodes
 {
     /// <summary>
